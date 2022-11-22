e5ef4699eea59b14888ce1dd4420c69b6e59628e
gs.piit130 <gs.piit130@kyowon.com>
2022-11-22 11:20:28 +0900

    fix: 시도명, 시군구명 수정

--- src/project/wells/web/service/pages/allocate/WwsncResponsibilityLocalAreaZipMgtM.vue
+++ src/project/wells/web/service/pages/allocate/WwsncResponsibilityLocalAreaZipMgtM.vue
@@ -44,26 +44,26 @@
         <!-- 광역시/도 -->
         <kw-search-item :label="$t('MSG_TXT_MGPO')">
           <kw-select
-            v-model="searchParams.fr2pLgldCd"
-            :options="sidos"
+            v-model="searchParams.ctpvNm"
+            :options="ctpvs"
             first-option="all"
-            first-option-label="- 전체 -"
+            :first-option-label="$t('MSG_TXT_ALL')"
             first-option-value=""
-            option-label="tryNm"
-            option-value="fr2pLgldCd"
-            @update:model-value="onUpdateSgg"
+            option-label="ctpv"
+            option-value="ctpvNm"
+            @update:model-value="onUpdateCtcty"
           />
         </kw-search-item>
         <!-- 시군구 -->
-        <kw-search-item :label="$t('MSG_TXT_SGG')">
+        <kw-search-item :label="$t('MSG_TXT_CTCTY')">
           <kw-select
-            v-model="searchParams.sggNm"
-            :options="sggs"
+            v-model="searchParams.ctctyNm"
+            :options="ctctys"
             first-option="all"
-            first-option-label="- 전체 -"
+            :first-option-label="$t('MSG_TXT_ALL')"
             first-option-value=""
-            option-label="sgg"
-            option-value="sggNm"
+            option-label="ctcty"
+            option-value="ctctyNm"
           />
         </kw-search-item>
       </kw-search-row>
@@ -72,10 +72,7 @@
         <kw-search-item :label="$t('MSG_TXT_WK_GRP')">
           <kw-select
             v-model="searchParams.wkGrpCd"
-            :options="commonCode.BD10"
-            first-option="all"
-            first-option-label="- 전체 -"
-            first-option-value=""
+            :options="codeBd10"
             option-label="label"
             option-value="value"
           />
@@ -102,6 +99,7 @@
           dense
           secondary
           :label="$t('MSG_BTN_SAVE')"
+          @click="onClickSave"
         />
         <kw-separator
           spaced
@@ -143,14 +141,16 @@
 // -------------------------------------------------------------------------------------------------
 // Import & Declaration
 // -------------------------------------------------------------------------------------------------
-import { codeUtil, defineGrid, getComponentType, useDataService, useMeta } from 'kw-lib';
+import { codeUtil, defineGrid, getComponentType, gridUtil, useDataService, useGlobal, useMeta } from 'kw-lib';
 import { cloneDeep } from 'lodash-es';
 import { getLcCommonCodeCo110tb, getLcAllocateAc112tb } from '~sms-wells/web/service/utils/common';
+import dayjs from 'dayjs';
 
 const { t } = useI18n();
 const dataService = useDataService();
 
 const { getConfig } = useMeta();
+const { alert, notify } = useGlobal();
 
 // -------------------------------------------------------------------------------------------------
 // Function & Event
@@ -161,10 +161,10 @@ let cachedParams;
 const searchParams = ref({
   zipFrom: '',
   zipTo: '',
-  fr2pLgldCd: '',
-  sggNm: '',
-  wkGrpCd: '',
-  applyDate: '',
+  ctpvNm: '',
+  ctctyNm: '',
+  wkGrpCd: '10',
+  applyDate: dayjs().format('YYYYMMDD'),
 });
 
 const pageInfo = ref({
@@ -176,9 +176,11 @@ const pageInfo = ref({
 const codes = await codeUtil.getMultiCodes('COD_PAGE_SIZE_OPTIONS');
 
 const commonCode = await getLcCommonCodeCo110tb();
+const codeBd10 = commonCode.BD10.sort((a, b) => Number(a.value) - Number(b.value));
+
 const ac112tb = await getLcAllocateAc112tb();
-const sidos = await getLcAllocateAc112tb('sido');
-const sggs = ref((await getLcAllocateAc112tb('gu')).map((v) => ({ sgg: v.sggNm, sggNm: v.sggNm })));
+const ctpvs = ref((await getLcAllocateAc112tb('sido')).map((v) => ({ ctpv: v.tryNm, ctpvNm: v.tryNm, ctpvCd: v.fr2pLgldCd })));
+const ctctys = ref((await getLcAllocateAc112tb('gu')).map((v) => ({ ctcty: v.sggNm, ctctyNm: v.sggNm })));
 
 async function fetchData() {
   const res = await dataService.get('/sms/wells/service/rpb-locara-zip-mngt', { params: { ...cachedParams, ...pageInfo.value } });
@@ -196,12 +198,31 @@ async function onClickSearch() {
 }
 
 async function onClickExcelDownload() {
-  console.log('excel download...');
+  const view = grdMainRef.value.getView();
+  if (view.getItemCount() === 0) {
+    await alert(t('MSG_ALT_NO_INFO_SRCH'));
+  }
 }
 
-const onUpdateSgg = async (val) => {
-  sggs.value = (await getLcAllocateAc112tb('gu', val)).map((v) => ({ sgg: v.sggNm, sggNm: v.sggNm }));
+const onUpdateCtcty = async (val) => {
+  if (val) {
+    const { ctpvCd } = ctpvs.value.find((v) => v.ctpvNm === val);
+    ctctys.value = (await getLcAllocateAc112tb('gu', ctpvCd)).map((v) => ({ ctcty: v.sggNm, ctctyNm: v.sggNm }));
+  }
 };
+
+async function onClickSave() {
+  const view = grdMainRef.value.getView();
+
+  if (view.getCheckedItems().length === 0) {
+    await notify(t('MSG_ALT_NO_APPY_OBJ_DT'));
+    return;
+  }
+  if (!await gridUtil.alertIfIsNotModified(view)) {
+    // await dataService.post('/sms/wells/service/rpb-locara-zip-mngt', )
+    await notify(t('MSG_ALT_SAVE_DATA'));
+  }
+}
 
 // -------------------------------------------------------------------------------------------------
 // Initialize Grid
@@ -211,8 +232,8 @@ const initGrdMain = defineGrid((data, view) => {
     { fieldName: 'newAdrZip' },
     { fieldName: 'mgtCnt' },
     { fieldName: 'wrkCnt' },
-    { fieldName: 'tryNm' },
-    { fieldName: 'sggNm' },
+    { fieldName: 'ctpvNm' },
+    { fieldName: 'ctctyNm' },
     { fieldName: 'lawcEmdNm' },
     { fieldName: 'amtdNm' },
     { fieldName: 'mngtAmtd' },
@@ -226,12 +247,12 @@ const initGrdMain = defineGrid((data, view) => {
 
   const columns = [
     { fieldName: 'newAdrZip', header: t('MSG_TXT_ZIP'), width: '100', styleName: 'text-center' },
-    { fieldName: 'mgtCnt', header: t('MSG_TXT_SV_ACC'), width: '100', styleName: 'text-center' },
-    { fieldName: 'wrkCnt', header: t('MSG_TXT_MLMN_ACTCS'), width: '100', styleName: 'text-center' },
-    { fieldName: 'tryNm', header: t('MSG_TXT_TRY_NM'), width: '150', styleName: 'text-center' },
-    { fieldName: 'sggNm', header: t('MSG_TXT_SGG_NM'), width: '150', styleName: 'text-center' },
-    { fieldName: 'lawcEmdNm', header: t('MSG_TXT_EMD_NM'), width: '150', styleName: 'text-center' },
-    { fieldName: 'amtdNm', header: t('MSG_TXT_AMTD_NM'), width: '150', styleName: 'text-center' },
+    { fieldName: 'mgtCnt', header: t('MSG_TXT_SV_ACC'), width: '100', styleName: 'text-right' },
+    { fieldName: 'wrkCnt', header: t('MSG_TXT_MLMN_ACTCS'), width: '100', styleName: 'text-right' },
+    { fieldName: 'ctpvNm', header: t('MSG_TXT_CTPV_NM'), width: '150', styleName: 'text-left' },
+    { fieldName: 'ctctyNm', header: t('MSG_TXT_CTCTY_NM'), width: '150', styleName: 'text-left' },
+    { fieldName: 'lawcEmdNm', header: t('MSG_TXT_EMD_NM'), width: '150', styleName: 'text-left' },
+    { fieldName: 'amtdNm', header: t('MSG_TXT_AMTD_NM'), width: '150', styleName: 'text-left' },
     {
       fieldName: 'mngtAmtd',
       header: t('MSG_TXT_MNGT_AMTD'),
@@ -240,11 +261,11 @@ const initGrdMain = defineGrid((data, view) => {
       optionValue: 'value',
       editor: { type: 'list' },
       editable: true,
-      styleName: 'text-center',
+      styleName: 'text-left',
       styleCallback: (grid, dataCell) => {
-        const sggNm = grid.getValue(dataCell.index.itemIndex, 'sggNm');
+        const ctctyNm = grid.getValue(dataCell.index.itemIndex, 'ctctyNm');
         const ac112MgtHemdNm = ac112tb
-          .filter((v) => v.sggNm === sggNm)
+          .filter((v) => v.sggNm === ctctyNm)
           .map((v) => v.ac112MgtHemdNm)
           .reduce((a, v) => (a.includes(v) ? a : [...a, v]), []);
 
@@ -253,10 +274,10 @@ const initGrdMain = defineGrid((data, view) => {
     },
     { fieldName: 'rpbLocaraCd', header: t('MSG_TXT_LOCARA_CMN_CD'), width: '100', styleName: 'text-center' },
     { fieldName: 'rpbLocaraGrpCd', header: t('MSG_TXT_LOCARA_GRP_CD'), width: '100', styleName: 'text-center' },
-    { fieldName: 'ogNm', header: t('MSG_TXT_CENTER_DIVISION'), width: '120', styleName: 'text-center' },
+    { fieldName: 'ogNm', header: t('MSG_TXT_CENTER_DIVISION'), width: '120', styleName: 'text-left' },
     { fieldName: 'ichrPrtnrNo', header: t('MSG_TXT_RPB_PRTNR_NO'), width: '120', styleName: 'text-center' },
-    { fieldName: 'prtnrKnm', header: t('MSG_TXT_RPB_ICHR_NM'), width: '100', styleName: 'text-center' },
-    { fieldName: 'vstDowVal', header: t('MSG_TXT_VST_DOW'), width: '100', styleName: 'text-center' },
+    { fieldName: 'prtnrKnm', header: t('MSG_TXT_RPB_ICHR_NM'), width: '100', styleName: 'text-left' },
+    { fieldName: 'vstDowVal', header: t('MSG_TXT_VST_DOW'), width: '100', styleName: 'text-left' },
   ];
 
   const columnLayout = [
@@ -266,8 +287,8 @@ const initGrdMain = defineGrid((data, view) => {
     {
       direction: 'horizontal',
       items: [
-        'tryNm',
-        'sggNm',
+        'ctpvNm',
+        'ctctyNm',
         'lawcEmdNm',
       ],
       header: {
@@ -288,11 +309,16 @@ const initGrdMain = defineGrid((data, view) => {
 
   data.setFields(fields);
   view.setColumns(columns);
+  view.setFixedOptions({ colCount: 1 });
 
   view.checkBar.visible = true;
   view.rowIndicator.visible = true;
   view.editOptions.columnEditableFirst = true;
 
   view.displayOptions.emptyMessage = t('MSG_ALT_NO_INFO_SRCH');
+
+  view.onCellEdited = (grid, itemIndex) => {
+    grid.checkItem(itemIndex, true);
+  };
 });
 </script>
