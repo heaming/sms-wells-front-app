c847c12a7be7f0042801b997815e8f6960a826bc
gs.piit130 <gs.piit130@kyowon.com>
2022-12-19 18:25:30 +0900

    feat: 급지 우편번호 관리 화면 추가

---
+++ src/project/wells/service/pages/orgcode/WwsndRegionLevelZipMgtM.vue
@@ -0,0 +1,350 @@
+<!----
+ ****************************************************************************************************
+ * 프로그램 개요
+ ****************************************************************************************************
+ 1. 모듈 : SND (조직관리)
+ 2. 프로그램 ID : W-SV-U-0145M01 - 급지 우편번호 관리
+ 3. 작성자 : gs.piit130
+ 4. 작성일 : 2022.12.06
+ ****************************************************************************************************
+ * 프로그램 설명
+ ****************************************************************************************************
+ - 급지 우편번호 관리 (http://localhost:3000/#/service/wwsnd-region-level-zip-mgt)
+ ****************************************************************************************************
+--->
+<template>
+  <kw-page>
+    <kw-search
+      @search="onClickSearch"
+    >
+      <kw-search-row>
+        <!-- 우편번호 -->
+        <kw-search-item :label="$t('MSG_TXT_ZIP')">
+          <kw-input
+            v-model="searchParams.zipFrom"
+            type="text"
+            maxlength="3"
+            :regex="/^[0-9]*$/i"
+          />
+          <span>~</span>
+          <kw-input
+            v-model="searchParams.zipTo"
+            type="text"
+            maxlength="3"
+            :regex="/^[0-9]*$/i"
+          />
+        </kw-search-item>
+        <!-- 광역시/도 -->
+        <kw-search-item :label="$t('MSG_TXT_MGPO')">
+          <kw-select
+            v-model="searchParams.ctpvNm"
+            :options="ctpvs"
+            first-option="all"
+            option-label="ctpv"
+            option-value="ctpvNm"
+            @update:model-value="onUpdateCtcty"
+          />
+        </kw-search-item>
+        <!-- 시군구 -->
+        <kw-search-item :label="$t('MSG_TXT_CTCTY')">
+          <kw-select
+            v-model="searchParams.ctctyNm"
+            :options="ctctys"
+            first-option="all"
+            option-label="ctcty"
+            option-value="ctctyNm"
+          />
+        </kw-search-item>
+      </kw-search-row>
+
+      <kw-search-row>
+        <!-- 작업그룹 -->
+        <kw-search-item :label="$t('MSG_TXT_WK_GRP')">
+          <kw-select
+            v-model="searchParams.wkGrpCd"
+            :options="codes.WK_GRP_CD"
+          />
+        </kw-search-item>
+        <!-- 서비스센터 -->
+        <kw-search-item :label="$t('MSG_TXT_SV_CNR')">
+          <kw-select
+            v-model="searchParams.ogId"
+            :options="serviceCenter"
+            first-option="all"
+            option-label="ogNm"
+            option-value="ogCd"
+          />
+        </kw-search-item>
+      </kw-search-row>
+    </kw-search>
+
+    <div class="result-area">
+      <kw-action-top>
+        <template #left>
+          <kw-paging-info
+            v-model:page-index="pageInfo.pageIndex"
+            v-model:page-size="pageInfo.pageSize"
+            :total-count="pageInfo.totalCount"
+            :page-size-options="codes.COD_PAGE_SIZE_OPTIONS"
+            @change="fetchData"
+          />
+        </template>
+        <kw-btn
+          dense
+          secondary
+          :label="$t('MSG_BTN_SAVE')"
+          @click="onClickSave"
+        />
+        <kw-btn
+          icon="download_on"
+          dense
+          secondary
+          :label="$t('MSG_BTN_EXCEL_DOWN')"
+          :disable="pageInfo.totalCount === 0"
+          @click="onClickExcelDownload"
+        />
+      </kw-action-top>
+
+      <kw-grid
+        ref="grdMainRef"
+        :visible-rows="pageInfo.pageSize"
+        @init="initGrdMain"
+      />
+
+      <kw-pagination
+        v-model:page-index="pageInfo.pageIndex"
+        v-model:page-size="pageInfo.pageSize"
+        :total-count="pageInfo.totalCount"
+        @change="fetchData"
+      />
+    </div>
+  </kw-page>
+</template>
+
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { codeUtil, defineGrid, getComponentType, gridUtil, useDataService, useMeta, useGlobal } from 'kw-lib';
+import { cloneDeep } from 'lodash-es';
+import useSnCode from '~sms-wells/service/composables/useSnCode';
+
+const { getDistricts, getServiceCenters } = useSnCode();
+
+const { t } = useI18n();
+const dataService = useDataService();
+
+const { getConfig } = useMeta();
+const { notify } = useGlobal();
+
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const grdMainRef = ref(getComponentType('KwGrid'));
+
+let cachedParams;
+const searchParams = ref({
+  zipFrom: '',
+  zipTo: '',
+  ctpvNm: '',
+  ctctyNm: '',
+  wkGrpCd: '10',
+  ogId: '',
+});
+
+const pageInfo = ref({
+  totalCount: 0,
+  pageIndex: 1,
+  pageSize: Number(getConfig('CFG_CMZ_DEFAULT_PAGE_SIZE')),
+});
+
+const codes = await codeUtil.getMultiCodes(
+  'COD_PAGE_SIZE_OPTIONS',
+  'WK_GRP_CD',
+);
+
+const serviceCenter = (await getServiceCenters()).filter((v) => v.ogNm !== null);
+const ctpvs = ref((await getDistricts('sido')).map((v) => ({ ctpv: v.ctpvNm, ctpvNm: v.ctpvNm, ctpvCd: v.fr2pLgldCd })));
+const ctctys = ref((await getDistricts('guAll')).map((v) => ({ ctcty: v.ctctyNm, ctctyNm: v.ctctyNm })));
+const cachedCtctys = cloneDeep(ctctys.value);
+
+let cachedZips;
+async function fetchData() {
+  const res = await dataService.get('/sms/wells/service/region-level-zipnos/paging', { params: { ...cachedParams, ...pageInfo.value } });
+  const { list: zips, pageInfo: pagingResult } = res.data;
+
+  cachedZips = cloneDeep(zips);
+
+  pageInfo.value = pagingResult;
+  const view = grdMainRef.value.getView();
+  view.getDataSource().setRows(zips);
+  view.resetCurrent();
+
+  if (pageInfo.value.totalCount === 0) {
+    await notify(t('MSG_ALT_NO_INFO_SRCH'));
+  }
+}
+
+async function onClickSearch() {
+  pageInfo.value.pageIndex = 1;
+  cachedParams = cloneDeep(searchParams.value);
+  await fetchData();
+}
+
+async function onClickExcelDownload() {
+  const view = grdMainRef.value.getView();
+
+  const res = await dataService.get('/sms/wells/service/region-level-zipnos/excel-download', { params: cachedParams });
+  await gridUtil.exportView(view, {
+    fileName: 'rpbLocaraZipList',
+    timePostfix: true,
+    exportData: res.data,
+  });
+}
+
+async function onUpdateCtcty(val) {
+  if (val) {
+    const { ctpvCd } = ctpvs.value.find((v) => v.ctpvNm === val);
+    ctctys.value = (await getDistricts('gu', ctpvCd)).map((v) => ({ ctcty: v.ctctyNm, ctctyNm: v.ctctyNm }));
+  } else {
+    ctctys.value = cachedCtctys;
+  }
+}
+
+async function onClickSave() {
+  const view = grdMainRef.value.getView();
+
+  if (view.getCheckedItems().length === 0) {
+    await notify(t('MSG_ALT_NO_APPY_OBJ_DT'));
+    return;
+  }
+
+  if (!await gridUtil.alertIfIsNotModified(view)) {
+    const changedRows = gridUtil.getChangedRowValues(view);
+
+    await dataService.put('/sms/wells/service/region-level-zipnos', changedRows);
+
+    await notify(t('MSG_ALT_SAVE_DATA'));
+    await fetchData();
+  }
+}
+
+// -------------------------------------------------------------------------------------------------
+// Initialize Grid
+// -------------------------------------------------------------------------------------------------
+const initGrdMain = defineGrid((data, view) => {
+  const fields = [
+    { fieldName: 'newAdrZip' },
+    { fieldName: 'mgtCnt' },
+    { fieldName: 'wrkCnt' },
+    { fieldName: 'ctpvNm' },
+    { fieldName: 'ctctyNm' },
+    { fieldName: 'lawcEmdNm' },
+    { fieldName: 'amtdNm' },
+    { fieldName: 'rpbLocaraCd' },
+    { fieldName: 'rpbLocaraGrpCd' },
+    { fieldName: 'ogNm' },
+    { fieldName: 'ichrPrtnrNo' },
+    { fieldName: 'prtnrKnm' },
+    { fieldName: 'vstDowVal' },
+    { fieldName: 'emdSn' },
+    { fieldName: 'fr2pLgldCd' },
+    { fieldName: 'pdlvNo' },
+    { fieldName: 'pdlvNm' },
+    { fieldName: 'pdlvAdr' },
+    { fieldName: 'ildYn' },
+    { fieldName: 'placeOfDeliveries' },
+  ];
+
+  const columns = [
+    { fieldName: 'newAdrZip', header: t('MSG_TXT_ZIP'), width: '100', styleName: 'text-center' },
+    { fieldName: 'mgtCnt', header: t('MSG_TXT_SV_ACC'), width: '100', styleName: 'text-right' },
+    { fieldName: 'wrkCnt', header: t('MSG_TXT_MLMN_ACTCS'), width: '100', styleName: 'text-right' },
+    { fieldName: 'ctpvNm', header: t('MSG_TXT_CTPV_NM'), width: '100' },
+    { fieldName: 'ctctyNm', header: t('MSG_TXT_CTCTY_NM'), width: '100' },
+    { fieldName: 'lawcEmdNm', header: t('MSG_TXT_EMD_NM'), width: '100' },
+    { fieldName: 'amtdNm', header: t('MSG_TXT_AMTD_NM'), width: '100' },
+    { fieldName: 'rpbLocaraCd', header: t('MSG_TXT_LOCARA_CMN_CD'), width: '100', styleName: 'text-center' },
+    { fieldName: 'rpbLocaraGrpCd', header: t('MSG_TXT_LOCARA_GRP_CD'), width: '100', styleName: 'text-center' },
+    { fieldName: 'ogNm', header: t('MSG_TXT_CENTER_DIVISION'), width: '150' },
+    { fieldName: 'ichrPrtnrNo', header: t('MSG_TXT_RPB_PRTNR_NO'), width: '100', styleName: 'text-center' },
+    { fieldName: 'prtnrKnm', header: t('MSG_TXT_RPB_ICHR'), width: '100' },
+    { fieldName: 'vstDowVal', header: t('MSG_TXT_VST_DOW'), width: '100' },
+    {
+      fieldName: 'pdlvNm',
+      header: t('MSG_TXT_AMTD_RSDT_CNR'),
+      width: '100',
+      editor: { type: 'list' },
+      editable: true,
+      styleCallback: (grid, dataCell) => {
+        const { placeOfDeliveries: pdlvs } = cachedZips[dataCell.index.itemIndex];
+        // const pdlvNo = pdlvs.map((v) => v.pdlvNo);
+        const pdlvNm = pdlvs.map((v) => v.pdlvNm);
+
+        return { editor: { type: 'list', labels: pdlvNm, values: pdlvNm } };
+      },
+    },
+    { fieldName: 'pdlvAdr', header: t('MSG_TXT_RSDT_CNR_ADR'), width: '100' },
+    { fieldName: 'ildYn', header: t('MSG_TXT_ILD_DV'), width: '100', styleName: 'text-center' },
+  ];
+
+  const columnLayout = [
+    'newAdrZip',
+    'mgtCnt',
+    'wrkCnt',
+    {
+      direction: 'horizontal',
+      items: [
+        'ctpvNm',
+        'ctctyNm',
+        'lawcEmdNm',
+      ],
+      header: {
+        text: t('MSG_TXT_LGLD'),
+      },
+    },
+    'amtdNm',
+    'rpbLocaraCd',
+    'rpbLocaraGrpCd',
+    'ogNm',
+    'ichrPrtnrNo',
+    'prtnrKnm',
+    'vstDowVal',
+    {
+      direction: 'horizontal',
+      items: [
+        'pdlvNm',
+        'pdlvAdr',
+        'ildYn',
+      ],
+      header: {
+        text: t('MSG_TXT_CEN_LOCARA'),
+      },
+    },
+  ];
+
+  view.setColumnLayout(columnLayout);
+
+  data.setFields(fields);
+  view.setColumns(columns);
+
+  view.checkBar.visible = true;
+  view.setCheckableCallback(() => false);
+  view.rowIndicator.visible = true;
+  view.editOptions.columnEditableFirst = true;
+
+  view.onGetEditValue = async (grid, index, editResult) => {
+    grid.checkItem(index.itemIndex, true);
+
+    const { placeOfDeliveries: pdlvs } = cachedZips[index.itemIndex];
+    const { pdlvNo, pdlvAdr } = pdlvs.find((v) => v.pdlvNm === editResult.value);
+
+    grid.setValue(index.dataRow, index.fieldName, editResult.value);
+    grid.setValue(index.dataRow, 'pdlvNo', pdlvNo);
+    grid.setValue(index.dataRow, 'pdlvAdr', pdlvAdr);
+
+    console.log(data.getJsonRow(index.dataRow));
+  };
+});
+
+</script>
