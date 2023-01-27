626e22dc7c7368e468ef9b633de0056e1218aeb7
gs.nidhi.d <gs.nidhi.d@kyowon.co.kr>
2023-01-27 10:57:30 +0530

    feat: added CRUD functionality

--- src/project/wells/contract/pages/orderstatus/WwctaRentalAccountMgtM.vue
+++ src/project/wells/contract/pages/orderstatus/WwctaRentalAccountMgtM.vue
@@ -14,41 +14,68 @@
 --->
 <template>
   <kw-page>
-    <kw-search>
+    <kw-search @search="onClickSearch">
       <kw-search-row>
         <kw-search-item :label="$t('MSG_TXT_INQR_DV')">
           <kw-option-group
+            v-model="srchSelection"
             type="radio"
-            :options="['상품별', '관리조직별']"
-            model-value="상품별"
+            :options="srchOptions"
+            @change="onChangeSearch"
           />
         </kw-search-item>
-
         <kw-search-item
           :label="$t('MSG_TXT_YR_INSTALLATION')"
           required
         >
           <kw-date-range-picker
+            v-model:from="searchParams.dangOcStrtdt[0]"
+            v-model:to="searchParams.dangOcStrtdt[1]"
             rules="date_range_required|date_range_months:1"
-            type="month"
           />
         </kw-search-item>
-        <kw-search-item :label="$t('MSG_TXT_PDGRP')">
+        <kw-search-item
+          v-if="srchSelection === 100"
+          :label="$t('MSG_TXT_PDGRP')"
+        >
           <kw-select
-            :model-value="[]"
+            v-model="searchParams.prdGrp"
             :options="['BO등록일', '입찰예정일']"
+            rules="required"
+          />
+        </kw-search-item>
+        <kw-search-item
+          v-else
+          :label="$t('MSG_TXT_MANAGEMENT_DEPARTMENT')"
+        >
+          <kw-select
+            v-model="searchParams.mngmntGrp"
+            :options="['A-Team', 'B-Team']"
             rules="required"
           />
         </kw-search-item>
       </kw-search-row>
       <kw-search-row>
-        <kw-search-item :label="$t('MSG_TXT_PRDT_CODE')">
-          <kw-input />
+        <kw-search-item
+          v-if="srchSelection ==='상품별'"
+          :label="$t('MSG_TXT_PRDT_CODE')"
+        >
+          <kw-input v-model="searchParams.prdCd" />
+        </kw-search-item>
+        <kw-search-item
+          v-else
+          :label="$t('MSG_TXT_RGNL_GRP')"
+        >
+          <kw-select
+            v-model="searchParams.rgnlGrp"
+            :options="['Grp1', 'Grp2']"
+            rules="required"
+          />
         </kw-search-item>
         <kw-search-item :label="$t('MSG_TXT_CST_DV')">
           <kw-select
-            :model-value="[]"
-            :options="['BO등록일', '입찰예정일']"
+            v-model="searchParams.cstmrDiv"
+            :options="codes.COPN_DV_CD"
           />
         </kw-search-item>
       </kw-search-row>
@@ -57,19 +84,22 @@
     <div class="result-area">
       <kw-action-top>
         <template #left>
-          <span>총</span>
-          <span class="accent pl4">900건</span>
+          <kw-paging-info
+            :total-count="totalCount"
+          />
         </template>
         <kw-btn
           icon="download_on"
           dense
           secondary
           :label="$t('MSG_BTN_EXCEL_DOWN')"
+          :disable="!totalCount"
           @click="onClickExcelDownload"
         />
       </kw-action-top>
       <kw-grid
-        :visible-rows="2"
+        ref="grdMainRef"
+        :visible-rows="totalCount"
         @init="initGrid"
       />
     </div>
@@ -81,10 +111,87 @@
 // -------------------------------------------------------------------------------------------------
 // Import & Declaration
 // -------------------------------------------------------------------------------------------------
+import { codeUtil, gridUtil, defineGrid, getComponentType, useDataService } from 'kw-lib';
+import { cloneDeep } from 'lodash-es';
+import dayjs from 'dayjs';
 
 const { t } = useI18n();
+const dataService = useDataService();
 
-function initGrid(data, view) {
+const now = dayjs();
+const totalCount = ref(10);
+const srchOptions = ref([{
+  codeId: 100,
+  codeName: '상품별' },
+{
+  codeId: 200,
+  codeName: '관리 조직별' }]);
+const srchSelection = ref(100);
+
+let cachedParams;
+const searchParams = ref({
+  dangOcStrtdt: [now.startOf('month').format('YYYYMMDD'), now.format('YYYYMMDD')],
+  prdGrp: '',
+  mngmntGrp: '',
+  prdCd: '',
+  rgnlGrp: '',
+  cstmrDiv: '',
+
+});
+
+const codes = await codeUtil.getMultiCodes(
+  'COPN_DV_CD',
+);
+
+const grdMainRef = ref(getComponentType('KwGrid'));
+
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const isProd = computed(() => srchSelection.value === 100);
+
+// Updating the col visibility as per search classification
+function onChangeSearch() {
+  const view = grdMainRef.value.getView();
+  view.columnsByTag('prod').forEach((col) => { col.visible = isProd.value; });
+  view.columnsByTag('org').forEach((col) => { col.visible = !(isProd.value); });
+}
+
+async function fetchData() {
+  // changing api & cacheparams according to search classification
+  let res = '';
+  if (isProd.value) {
+    const { mngmntGrp, rgnlGrp, ...prodParams } = cachedParams;
+    res = await dataService.get('/sms/wells/contract/product-rental-accounts', { params: prodParams });
+  } else {
+    const { prdGrp, prdCd, ...orgParams } = cachedParams;
+    res = await dataService.get('/sms/wells/contract/organization-rental-accounts', { params: orgParams });
+  }
+
+  const { list: accounts } = res.data;
+  totalCount.value = accounts.length;
+  const view = grdMainRef.value.getView();
+  view.getDataSource().setRows(accounts);
+  view.resetCurrent();
+}
+
+async function onClickSearch() {
+  cachedParams = cloneDeep(searchParams.value);
+  await fetchData();
+}
+
+async function onClickExcelDownload() {
+  const view = grdMainRef.value.getView();
+  await gridUtil.exportView(view, {
+    fileName: 'dataServiceManageList',
+    timePostfix: true,
+  });
+}
+
+// -------------------------------------------------------------------------------------------------
+// Initialize Grid
+// -------------------------------------------------------------------------------------------------
+const initGrid = defineGrid((data, view) => {
   const fields = [
     { fieldName: 'col1' },
     { fieldName: 'col2' },
@@ -99,22 +206,27 @@ function initGrid(data, view) {
     { fieldName: 'col11' },
     { fieldName: 'col12' },
     { fieldName: 'col13' },
+    { fieldName: 'col14' },
+    { fieldName: 'col15' },
+
   ];
 
   const columns = [
-    { fieldName: 'col1', header: t('PDGRP'), width: '178' },
-    { fieldName: 'col2', header: t('PD_NM'), width: '295' },
-    { fieldName: 'col3', header: t('PRDT_CODE'), width: '125', styleName: 'text-center' },
-    { fieldName: 'col4', header: t('YR_INSTALLATION'), width: '125', styleName: 'text-center' },
-    { fieldName: 'col5', header: t('Recommitment'), width: '125', styleName: 'text-center' },
-    { fieldName: 'col6', header: t('SUBSCRPTN_NO'), width: '139', styleName: 'text-right' },
-    { fieldName: 'col7', header: t('CHNG'), width: '139', styleName: 'text-right' },
-    { fieldName: 'col8', header: t('RE_RENTAL'), width: '139', styleName: 'text-right' },
-    { fieldName: 'col9', header: t('MEMBERSHIP'), width: '139', styleName: 'text-right' },
-    { fieldName: 'col10', header: t('RNTL_MNTENC'), width: '139', styleName: 'text-right' },
+    { fieldName: 'col1', header: t('MSG_TXT_PDGRP'), width: '178', visible: true, tag: 'prod' },
+    { fieldName: 'col2', header: t('MSG_TXT_PD_NM'), width: '295', visible: true, tag: 'prod' },
+    { fieldName: 'col3', header: t('MSG_TXT_PRDT_CODE'), width: '125', styleName: 'text-center', visible: true, tag: 'prod' },
+    { fieldName: 'col14', header: t('MSG_TXT_MANAGEMENT_DEPARTMENT'), width: '178', visible: false, tag: 'org' },
+    { fieldName: 'col15', header: t('MSG_TXT_RGNL_GRP'), width: '295', visible: false, tag: 'org' },
+    { fieldName: 'col4', header: t('MSG_TXT_YR_INSTALLATION'), width: '125', styleName: 'text-center' },
+    { fieldName: 'col5', header: t('MSG_TXT_RECOMMITMENT'), width: '125', styleName: 'text-center' },
+    { fieldName: 'col6', header: t('MSG_TXT_SUBSCRPTN_NO'), width: '139', styleName: 'text-right' },
+    { fieldName: 'col7', header: t('MSG_TXT_CHNG'), width: '139', styleName: 'text-right' },
+    { fieldName: 'col8', header: t('MSG_TXT_RE_RENTAL'), width: '139', styleName: 'text-right' },
+    { fieldName: 'col9', header: t('MSG_TXT_MEMBERSHIP'), width: '139', styleName: 'text-right' },
+    { fieldName: 'col10', header: t('MSG_TXT_RNTL_MNTENC'), width: '139', styleName: 'text-right' },
     { fieldName: 'col11', header: '이탈(만료→자가)', width: '171', styleName: 'text-right' },
     { fieldName: 'col12', header: '이탈(철거→후속계약X)', width: '194', styleName: 'text-right' },
-    { fieldName: 'col13', header: t('WTHDRWL_RT'), width: '139', styleName: 'text-right' },
+    { fieldName: 'col13', header: t('MSG_TXT_WTHDRWL_RT'), width: '139', styleName: 'text-right' },
   ];
 
   data.setFields(fields);
@@ -123,10 +235,11 @@ function initGrid(data, view) {
   view.rowIndicator.visible = true;
 
   data.setRows([
-    { col1: '공기청정기', col2: '공기청정기AK316ESA', col3: '4375', col4: '2022-10', col5: 'N', col6: '1', col7: '0', col8: '0', col9: '0', col10: '0', col11: '0', col12: '0', col13: '0' },
-    { col1: '비데', col2: '비데(BM750RWA)', col3: '4416', col4: '2022-10', col5: 'N', col6: '6', col7: '0', col8: '0', col9: '0', col10: '0', col11: '0', col12: '0', col13: '0' },
+    { col1: '공기청정기', col2: '공기청정기AK316ESA', col3: '4375', col4: '2022-10', col5: 'N', col6: '1', col7: '0', col8: '0', col9: '0', col10: '0', col11: '0', col12: '0', col13: '0', col14: 'Business', col15: 'Grp1' },
+    { col1: '비데', col2: '비데(BM750RWA)', col3: '4416', col4: '2022-10', col5: 'N', col6: '6', col7: '0', col8: '0', col9: '0', col10: '0', col11: '0', col12: '0', col13: '0', col14: 'Business', col15: 'Grp2' },
+    { col1: '비데', col2: '비데(BM750RWA)', col3: '4416', col4: '2022-10', col5: 'N', col6: '6', col7: '0', col8: '0', col9: '0', col10: '0', col11: '0', col12: '0', col13: '0', col14: 'Business', col15: 'Grp2' },
   ]);
-}
+});
 
 </script>
 
