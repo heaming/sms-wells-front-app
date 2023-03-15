5885d8b21cdf922b310d890bb2b9a2e0c992b848
김대영 <gs.piit40@kyowon.co.kr>
2023-03-15 16:24:23 +0900

    fix: tablet entry 구성

---
+++ src/entries/tablet/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <tablet-layout>
+    <template #default>
+      <tablet-footer />
+      <tablet-left-drawer />
+      <tablet-stack-view />
+    </template>
+    <template #unauthenticated>
+      <tablet-fallback-login
+        tenant-id="TNT_WELLS"
+        portal-id="MBL_DEF"
+      />
+    </template>
+  </tablet-layout>
+</template>
+
+<script setup>
+import {
+  useSession,
+  TabletLayout, TabletFooter, TabletLeftDrawer, TabletStackView, TabletFallbackLogin,
+} from 'kw-lib';
+
+const {
+  isReady,
+} = useSession();
+
+await isReady();
+</script>
