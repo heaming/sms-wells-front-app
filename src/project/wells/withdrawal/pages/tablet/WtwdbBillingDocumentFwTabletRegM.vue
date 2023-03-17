ee1272fefd941fbffa83b221260612e80c1e4848
heungjun.lee <gs.piit168@kyowon.com>
2023-03-17 09:58:08 +0900

    수납 - 개별수납 - 테블릿 신규 등록
    Signed-off-by: heungjun.lee <gs.piit168@kyowon.com>

---
+++ src/project/wells/withdrawal/pages/tablet/WtwdbBillingDocumentFwTabletRegM.vue
@@ -0,0 +1,343 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : idvrve
+2. 프로그램 ID : WtwdbBillingDocumentFwTabletRegM - 청구서 발송 태블릿
+3. 작성자 : heungjun.lee
+4. 작성일 : 2023.02.07
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+-- 청구서 관리 조회
+****************************************************************************************************
+--->
+
+<template>
+  <kw-page>
+    <!-- 발송유형 -->
+    <h3 class="mb20">
+      {{ t('MSG_TXT_FW_TP') }}
+    </h3>
+
+    <kw-form
+      :cols="2"
+    >
+      <kw-form-row>
+        <kw-form-item>
+          <kw-btn-toggle
+            v-model="sendMainData.bildcFwTpCd"
+            :options="codes.BILDC_FW_TP_CD"
+            :dense="false"
+            gap="0"
+            grow
+            @change="fetchData"
+          />
+        </kw-form-item>
+      </kw-form-row>
+      <kw-form-row>
+        <!-- label="발신번호" -->
+        <kw-form-item
+          v-if="sendMainData.bildcFwTpCd === 'K'"
+          :label="t('MSG_TXT_DSPH_NO')"
+        >
+          <zwcm-telephone-number
+            v-model:tel-no1="telNos.telNo1"
+            v-model:tel-no2="telNos.telNo2"
+            v-model:tel-no3="telNos.telNo3"
+            required
+          />
+        </kw-form-item>
+        <!-- label="발신자" -->
+        <kw-form-item
+          v-else
+          :label="t('MSG_TXT_DSPTR')"
+        >
+          <zwcm-email-address
+            v-model="sendMainData.toMail"
+            required
+          />
+        </kw-form-item>
+        <!-- label="수신번호" -->
+        <kw-form-item
+          v-if="sendMainData.bildcFwTpCd === 'K'"
+          :label="t('MSG_TXT_RECP_NO')"
+        >
+          <zwcm-telephone-number
+            v-model:tel-no1="telNos2.telNo1"
+            v-model:tel-no2="telNos2.telNo2"
+            v-model:tel-no3="telNos2.telNo3"
+            required
+          />
+        </kw-form-item>
+        <!-- label="수신자" -->
+        <kw-form-item
+          v-else
+          :label="t('MSG_TXT_RECP_USR')"
+        >
+          <zwcm-email-address v-model="sendMainData.fromMail" />
+        </kw-form-item>
+      </kw-form-row>
+    </kw-form>
+
+    <kw-separator />
+
+    <!-- 발신 내역 -->
+    <h3>
+      {{ t('MSG_TXT_DSPH_IZ') }}
+    </h3>
+
+    <kw-list
+      :items="items"
+      separator
+      item-padding="16px 0"
+    >
+      <template #item="{ item }">
+        <kw-item-section>
+          <kw-form
+            dense
+          >
+            <kw-form
+              dense
+            >
+              <kw-form-item label="발송일시">
+                <p>{{ item.bildcFwDtm }}</p>
+              </kw-form-item>
+              <!-- label="발송유형" -->
+              <kw-form-item
+                :label="t('MSG_TXT_FW_TP')"
+              >
+                <p>{{ item.bildcFwTpCd }}</p>
+              </kw-form-item>
+              <!-- label="발신" -->
+              <kw-form-item
+                :label="t('MSG_TXT_DSPTR')"
+              >
+                <p>{{ item.callback }}</p>
+              </kw-form-item>
+              <!-- label="수신" -->
+              <kw-form-item
+                :label="t('MSG_TXT_RECP_USR')"
+              >
+                <p>{{ item.recipientNum }}</p>
+              </kw-form-item>
+            </kw-form>
+          </kw-form>
+        </kw-item-section>
+      </template>
+    </kw-list>
+
+    <template #action>
+      <div class="tablet-action-left">
+        <!-- label="이전" -->
+        <kw-btn
+          secondary
+          :label="t('MSG_BTN_CANCEL')"
+          @click="onClickBefore"
+        />
+      </div>
+      <!-- label="발송" -->
+      <div class="tablet-action-right">
+        <kw-btn
+          primary
+          :label="t('MSG_BTN_SEND')"
+          @click="onClickSend"
+        />
+      </div>
+    </template>
+  </kw-page>
+</template>
+
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { codeUtil, notify, useDataService, alert, confirm } from 'kw-lib';
+import ZwcmTelephoneNumber from '~common/components/ZwcmTelephoneNumber.vue';
+import ZwcmEmailAddress from '~common/components/ZwcmEmailAddress.vue';
+import { cloneDeep } from 'lodash-es';
+import dayjs from 'dayjs';
+
+const router = useRouter();
+const dataService = useDataService();
+const { t } = useI18n();
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const props = defineProps({
+
+  searchCstFnm: {
+    type: String,
+    default: null,
+  },
+  searchBildcWrteDt: {
+    type: String,
+    default: null,
+  },
+  bildcPblNo: {
+    type: String,
+    default: null,
+  },
+  cstFnm: {
+    type: String,
+    default: null,
+  },
+  pdNm: {
+    type: String,
+    default: null,
+  },
+  pdQtySum: {
+    type: String,
+    default: null,
+  },
+  pdSellAmtSum: {
+    type: String,
+    default: null,
+  },
+
+});
+
+const codes = await codeUtil.getMultiCodes(
+  'BILDC_FW_TP_CD',
+);
+
+const sendMainData = ref({
+  bildcPblNo: '',
+  bildcFwTpCd: 'K',
+  cstFnm: '',
+  pdNm: '',
+  pdQtySum: '',
+  pdSellAmtSum: '',
+  destInfo: '', // 받는사람
+  callback: '', // 보내는사람
+  fromMail: '',
+  toMail: '',
+
+});
+
+const items = ref([]);
+
+const telNos = ref({
+  telNo1: '',
+  telNo2: '',
+  telNo3: '',
+});
+
+const telNos2 = ref({
+  telNo1: '',
+  telNo2: '',
+  telNo3: '',
+});
+
+// 이전 버튼 클릭
+async function onClickBefore() {
+  await router.push(
+    {
+      path: '/ns/wtwdb-billing-document-tablet-mgt',
+      query: {
+        searchCstFnm: props.searchCstFnm, // 조회조건
+        searchBildcWrteDt: props.searchBildcWrteDt, // 조회조건
+      },
+
+    },
+  );
+}
+
+let cachedParams;
+
+async function fetchData() {
+  cachedParams = cloneDeep(sendMainData.value);
+
+  const res = await dataService.get('/sms/wells/withdrawal/idvrve/billing-document-orders/forwardings', { params: cachedParams });
+  console.log(res.data);
+
+  const list = res.data;
+
+  list.forEach((data) => {
+    if (data.bildcFwDtm) {
+      data.bildcFwDtm = dayjs(data.bildcFwDtm).format('YYYY-MM-DD');
+    }
+
+    if (data.bildcFwTpCd) {
+      if (data.bildcFwTpCd === 'K') {
+        // data.bildcFwTpCd = '카카오 알림톡';
+        data.bildcFwTpCd = t('MSG_TXT_KAKAO_NOTAK');
+      } else {
+        // data.bildcFwTpCd = '이메일';
+        data.bildcFwTpCd = t('MSG_TXT_EMAIL');
+      }
+    }
+  });
+
+  items.value = list;
+
+  console.log(items.value);
+}
+
+let paramData;
+async function onClickSend() {
+  if (await confirm(t('MSG_ALT_WANT_SEND?'))) {
+    sendMainData.value.destInfo = telNos.value.telNo1 + telNos.value.telNo2 + telNos.value.telNo3;
+    sendMainData.value.callback = telNos2.value.telNo1 + telNos2.value.telNo2 + telNos2.value.telNo3;
+
+    if (sendMainData.value.bildcFwTpCd === 'K') {
+      if (!telNos.value.telNo1 || !telNos.value.telNo2 || !telNos.value.telNo3) {
+        await alert(t('MSG_ALT_NCELL_REQUIRED_ITEM', [t('MSG_TXT_DSPH_NO')]));
+        return;
+      }
+
+      if (!telNos2.value.telNo1 || !telNos2.value.telNo2 || !telNos2.value.telNo3) {
+        await alert(t('MSG_ALT_NCELL_REQUIRED_ITEM', [t('MSG_TXT_RECP_NO')]));
+        return;
+      }
+    } else {
+    // eslint-disable-next-line no-lonely-if
+      if (!sendMainData.value.toMail) {
+        await alert(t('MSG_ALT_NCELL_REQUIRED_ITEM', [t('MSG_TXT_DSPTR_EML')]));
+        return;
+      }
+      if (!sendMainData.value.fromMail) {
+        await alert(t('MSG_ALT_NCELL_REQUIRED_ITEM', [t('MSG_TXT_RCVR_EML')]));
+        return;
+      }
+
+      // if (!sendMainData.value.fromMail) {
+      //   await alert(t('MSG_ALT_NCELL_REQUIRED_ITEM', [t('MSG_TXT_RCVR_EML')]));
+      //   return;
+      // }
+
+    // if (!sendMainData.value.toMail) {
+    //   await alert(t('MSG_ALT_NCELL_REQUIRED_ITEM', [t('MSG_TXT_DSPTR_EML')]));
+    //   return;
+    // }
+    }
+
+    paramData = cloneDeep(sendMainData.value);
+
+    await dataService.post('/sms/wells/withdrawal/idvrve/billing-document-orders/forwardings', paramData);
+
+    notify(t('MSG_ALT_SAVE_DATA')); // 메시지 자원 수정 필요
+
+    await fetchData();
+  }
+}
+
+async function initProps() {
+  const { bildcPblNo, cstFnm, pdNm, pdQtySum, pdSellAmtSum } = props;
+
+  sendMainData.value.bildcPblNo = bildcPblNo;
+  sendMainData.value.cstFnm = cstFnm;
+  sendMainData.value.pdNm = pdNm;
+  sendMainData.value.pdQtySum = pdQtySum;
+  sendMainData.value.pdSellAmtSum = pdSellAmtSum;
+
+  if (sendMainData.value.bildcPblNo) {
+    await fetchData();
+  }
+}
+
+onMounted(async () => {
+  await initProps();
+});
+
+</script>
---
+++ src/project/wells/withdrawal/pages/tablet/WtwdbBillingDocumentTabletMgtM.vue
@@ -0,0 +1,384 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : idvrve
+2. 프로그램 ID : WtwdbBillingDocumentTabletMgtM  - 청구서 관리 태블릿
+3. 작성자 : heungjun.lee
+4. 작성일 : 2023.02.07
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+-- 청구서 관리 조회
+****************************************************************************************************
+--->
+<template>
+  <kw-page
+    ref="pageRef"
+    @load="onLoad"
+  >
+    <template #etc>
+      <!-- label="청구서 작성" -->
+      <kw-btn
+        :label="t('MSG_BTN_BILDC_WRTE')"
+        borderless
+        @click="onClickRegP"
+      />
+    </template>
+    <!-- one-row -->
+    <kw-search
+      @search="onClickSearch"
+    >
+      <kw-search-row>
+        <kw-search-item>
+          <!-- required -->
+          <!-- rules="required" -->
+          <!-- label="고객명"             -->
+          <kw-input
+            v-model="searchParams.cstFnm"
+            :label="t('MSG_TXT_CST_NM')"
+            icon="search"
+            @click-icon="onClickSearchUser"
+          />
+        </kw-search-item>
+        <kw-search-item>
+          <kw-date-picker
+            v-model="searchParams.bildcWrteDt"
+            :label="t('MSG_TIT_DRAT_DT')"
+          />
+          <!-- label="작성일" -->
+        </kw-search-item>
+      </kw-search-row>
+    </kw-search>
+    <div class="result-area pt40">
+      <kw-action-top class="mt0">
+        <template #left>
+          <kw-paging-info
+            v-if="!isEditChk"
+            v-model:page-index="pageInfo.pageIndex"
+            v-model:page-size="pageInfo.pageSize"
+            :total-count="pageInfo.totalCount"
+            :page-size-options="codes.COD_PAGE_SIZE_OPTIONS"
+          />
+        </template>
+        <!-- label="편집" -->
+        <kw-btn
+          v-if="!isEditChk"
+          :label="t('MSG_BTN_EDIT')"
+          secondary
+          dense
+          @click="onClickedit"
+        />
+      </kw-action-top>
+      <kw-separator
+        v-if="!isEditChk"
+        color="black1"
+        spaced="0"
+      />
+      <kw-list
+        v-model:selected="arrModel"
+        :items="items"
+        separator
+        :checkbox="isEditChk"
+        item-padding="16px 0"
+        :clickable="true"
+      >
+        <!-- @click-item="onClickModify" -->
+        <!-- :clickable="!isEditChk" -->
+        <template #item="{ item }">
+          <kw-item-section>
+            <kw-form
+              dense
+            >
+              <kw-form
+                dense
+              >
+                <kw-form-row>
+                  <!-- label="고객명" -->
+                  <kw-form-item
+                    :label="t('MSG_TXT_CST_NM')"
+                  >
+                    >
+                    <p>{{ item.cstFnm }}</p>
+                  </kw-form-item>
+                  <!-- label="작성일자" -->
+                  <kw-form-item
+                    :label="t('MSG_TXT_WRTE_DT')"
+                  >
+                    <p>{{ item.bildcWrteDt }}</p>
+                  </kw-form-item>
+                  <!-- label="발송일자" -->
+                  <kw-form-item
+                    :label="t('MSG_TXT_FW_DT')"
+                  >
+                    <p>{{ item.bildcFwDtm }}</p>
+                  </kw-form-item>
+                </kw-form-row>
+                <kw-form-row>
+                  <!-- label="상품명" -->
+                  <kw-form-item
+                    :label="t('MSG_TXT_PRDT_NM')"
+                  >
+                    <p>{{ item.pdNm }}</p>
+                  </kw-form-item>
+                  <!-- label="총수량" -->
+                  <kw-form-item
+                    :label="t('MSG_TXT_TOT_QTY')"
+                  >
+                    <p>{{ item.pdQtySum }}</p>
+                  </kw-form-item>
+                  <!-- label="총금액" -->
+                  <kw-form-item
+                    :label="t('MSG_TXT_TOT_AMT')"
+                  >
+                    <p>{{ stringUtil.getNumberWithComma(toInteger(item.pdSellAmtSum)) }}</p>
+                  </kw-form-item>
+                </kw-form-row>
+              </kw-form>
+            </kw-form>
+          </kw-item-section>
+          <kw-item-section
+            side
+            top
+          >
+            <kw-btn
+              label="청구서 발송"
+              secondary
+              dense
+              @click="onClickMove(item)"
+            />
+          </kw-item-section>
+        </template>
+      </kw-list>
+    </div>
+
+    <template #action>
+      <div class="tablet-action-left">
+        <kw-btn
+          v-if="isEditChk"
+          secondary
+          :label="t('MSG_BTN_CANCEL')"
+          @click="onClickCencel"
+        />
+        <!-- label="취소" -->
+      </div>
+      <div class="tablet-action-right">
+        <kw-btn
+          v-if="isEditChk"
+          primary
+          :label="`${checkCount}`+t('MSG_BTN_DEL')"
+          @click="onClickRemove"
+        />
+        <!-- label="삭제" -->
+      </div>
+    </template>
+  </kw-page>
+</template>
+
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+
+import dayjs from 'dayjs';
+import { codeUtil, modal, useDataService, stringUtil, notify, confirm } from 'kw-lib';
+import { cloneDeep, isUndefined, toInteger } from 'lodash-es';
+
+const dataService = useDataService();
+const now = dayjs();
+const router = useRouter();
+const { t } = useI18n();
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const props = defineProps({
+  searchCstFnm: {
+    type: String,
+    default: null,
+  },
+  searchBildcWrteDt: {
+    type: String,
+    default: null,
+  },
+});
+
+const codes = await codeUtil.getMultiCodes(
+  'COD_PAGE_SIZE_OPTIONS',
+);
+
+const pageRef = ref();
+
+const items = ref([]);
+
+const isEditChk = ref(false);
+
+const arrModel = ref([]);
+
+const checkCount = ref([]);
+
+const pageInfo = ref({
+  totalCount: 0,
+  pageIndex: 1,
+  pageSize: Number(codes.COD_PAGE_SIZE_OPTIONS[0].codeName),
+  needTotalCount: true,
+});
+
+const searchParams = ref({
+  cstFnm: '',
+  bildcWrteDt: now.format('YYYYMMDD'),
+});
+
+let cachedParams;
+
+watch(() => arrModel.value.length, (newVal) => {
+  if (newVal === 0 || isUndefined(newVal)) {
+    checkCount.value = [];
+  } else {
+    checkCount.value = t('MSG_TXT_TASKS_CNT', [newVal]);
+  }
+});
+
+async function onLoad(pageIdx) {
+  pageInfo.value.pageIndex = pageIdx;
+  cachedParams = { ...cachedParams, ...pageInfo.value };
+
+  const res = await dataService.get('/sms/wells/withdrawal/idvrve/billing-document-orders/paging', { params: cachedParams });
+  const { list: pages, pageInfo: pagingResult } = res.data;
+
+  pageInfo.value = pagingResult;
+
+  pages.forEach((data) => {
+    if (data.bildcWrteDt) {
+      data.bildcWrteDt = dayjs(data.fstRgstDtm).format('YYYY-MM-DD');
+    }
+
+    if (data.bildcFwDtm) {
+      data.bildcFwDtm = dayjs(data.bildcFwDtm).format('YYYY-MM-DD');
+    }
+  });
+
+  items.value.push(...pages);
+
+  // 가져온 데이터가 가져와야할 pageSize보다 작으면 마지막 페이지이다.
+  if (items.value.length >= pagingResult.totalCount) {
+    await pageRef.value.stopLoad();
+  } else {
+    await pageRef.value.startLoad();
+  }
+}
+
+async function onClickSearch() {
+  items.value = [];
+  arrModel.value = [];
+
+  pageInfo.value.pageIndex = 1;
+
+  cachedParams = cloneDeep(searchParams.value);
+
+  await onLoad();
+}
+
+// 고객명 찾기 이벤트
+async function onClickSearchUser() {
+  const { result, payload } = await modal({ component: 'ZwcsaCustomerListP' });
+
+  if (result) {
+    searchParams.value.cstFnm = payload.name;
+  }
+}
+
+// 청구서 작성
+async function onClickRegP() {
+  router.replace(
+    {
+      path: '/ns/wtwdb-billing-document-write-tablet-reg',
+      query: {
+        searchCstFnm: searchParams.value.cstFnm, // 조회조건
+        searchBildcWrteDt: searchParams.value.bildcWrteDt, // 조회조건
+      },
+    },
+  );
+}
+
+// 청구서 작성
+// async function onClickModify(data) {
+//   const modifyRows = cloneDeep(data.value);
+//   router.replace(
+//     {
+//       path: '/ns/wtwdb-billing-document-write-tablet-reg',
+//       query: {
+//         bildcPblNo: modifyRows.bildcPblNo,
+//         cstFnm: modifyRows.cstFnm,
+//         bildcWrteDt: dayjs(modifyRows.bildcWrteDt).format('YYYYMMDD'),
+//         bildcPblSn: modifyRows.bildcPblSn,
+//         sellPrtnrNo: modifyRows.sellPrtnrNo,
+//         sellPrtnrNm: modifyRows.sellPrtnrNm,
+//         searchCstFnm: searchParams.value.cstFnm, // 조회조건
+//         searchBildcWrteDt: searchParams.value.bildcWrteDt, // 조회조건
+//       },
+//       params: { a: 'b' },
+//     },
+//   );
+// }
+
+// 청구서발송
+async function onClickMove(data) {
+  router.replace(
+    {
+      path: '/ns/wtwdb-billing-document-fw-tablet-reg',
+      query: {
+        searchCstFnm: searchParams.value.cstFnm, // 조회조건
+        searchBildcWrteDt: searchParams.value.bildcWrteDt, // 조회조건
+        bildcPblNo: data.bildcPblNo,
+        cstFnm: data.cstFnm,
+        pdNm: data.pdNm,
+        pdQtySum: data.pdQtySum,
+        pdSellAmtSum: data.pdSellAmtSum,
+      },
+    },
+  );
+}
+
+// 편집 클릭
+async function onClickedit() {
+  isEditChk.value = !isEditChk.value;
+}
+
+// 취소 버튼
+async function onClickCencel() {
+  isEditChk.value = !isEditChk.value;
+}
+
+// 삭제 버튼
+async function onClickRemove() {
+  if (!await confirm(t('MSG_ALT_WANT_DEL'))) { return; }
+
+  const deletedRows = cloneDeep(arrModel.value);
+
+  // deleteCount.value = deleteData.length
+  if (deletedRows.length > 0) {
+    await dataService.delete('/sms/wells/withdrawal/idvrve/billing-document-orders', { data: deletedRows });
+    await onClickSearch();
+  }
+
+  notify(t('MSG_ALT_DELETED'));
+}
+
+async function initProps() {
+  if (props.searchCstFnm || props.searchBildcWrteDt) {
+    const { searchCstFnm, searchBildcWrteDt } = props;
+    searchParams.value.cstFnm = searchCstFnm;
+    searchParams.value.bildcWrteDt = searchBildcWrteDt;
+  }
+  await onClickSearch();
+}
+
+onMounted(async () => {
+  await initProps();
+});
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
