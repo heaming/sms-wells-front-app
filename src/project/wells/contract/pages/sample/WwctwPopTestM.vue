29a868839a81dbf728ac604b74fb94d5f357463c
gs.piit159 <gs.piit159@kyowon.com>
2023-03-31 13:54:08 +0900

    fix: 계약번호 조회 팝업 상세계약번호 조회조건 추가

--- src/project/wells/contract/pages/sample/WwctwPopTestM.vue
+++ src/project/wells/contract/pages/sample/WwctwPopTestM.vue
@@ -13,58 +13,25 @@
 ****************************************************************************************************
 --->
 <template>
-  <kw-page class="kw-guide">
-    <div class="kw-guide-section">
-      <q-card>
-        <div class="kw-guide-example">
-          <template
-            v-for="(pop, i) of pops"
-            :key="i"
-          >
-            <kw-expansion-item>
-              <template #header>
-                <kw-item-section>
-                  <kw-item-label>
-                    <span class="text-bold kw-font-pt18">{{ pop.pname }} - {{ pop.title }}</span>
-                  </kw-item-label>
-                </kw-item-section>
-              </template>
-              <kw-form cols="2">
-                <kw-form-row>
-                  <kw-form-item
-                    label="param"
-                    :colspan="2"
-                  >
-                    <kw-input
-                      v-model="popInfo[pop.pname].prm"
-                      type="textarea"
-                    />
-                  </kw-form-item>
-                </kw-form-row>
-                <kw-btn
-                  class="mr8"
-                  min-width="100%"
-                  primary
-                  label="OPEN POPUP"
-                  @click="openPop(pop.pname)"
-                />
-                <kw-form-row>
-                  <kw-form-item
-                    label="return"
-                    :colspan="2"
-                  >
-                    <kw-input
-                      type="textarea"
-                      :model-value="popInfo[pop.pname].rtn"
-                    />
-                  </kw-form-item>
-                </kw-form-row>
-              </kw-form>
-            </kw-expansion-item>
-            <kw-separator class="my20" />
-          </template>
-        </div>
-      </q-card>
+  <kw-page>
+    <div class="result-area">
+      <kw-list
+        item-padding="4px 0"
+      >
+        <kw-expansion-item
+          v-for="(pop, idx) in pops"
+          :key="`expansion-${idx}`"
+          group="test-item"
+          :label="pop.title"
+          header-class="text-bold kw-font-subtitle bg-bg-box px8"
+          block-inherit-padding
+        >
+          <zctz-popup-test-item
+            :popup="pop.pname"
+            :params="pop.param"
+          />
+        </kw-expansion-item>
+      </kw-list>
     </div>
   </kw-page>
 </template>
@@ -73,9 +40,8 @@
 // -------------------------------------------------------------------------------------------------
 // Import & Declaration
 // -------------------------------------------------------------------------------------------------
-import { useGlobal } from 'kw-lib';
+import ZctzPopupTestItem from '~sms-common/contract/components/ZctzPopupTestItem.vue';
 
-const { modal } = useGlobal();
 // -------------------------------------------------------------------------------------------------
 // Function & Event
 // -------------------------------------------------------------------------------------------------
@@ -84,12 +50,14 @@ const pops = [
     pname: 'WwctaContractNumberListP',
     title: '계약번호 조회(wells)',
     param: {
-      cntrCstKnm: '계약자',
-      istCstKnm: '설치자',
-      cralLocaraTno: '010',
-      mexnoEncr: '1234',
-      cralIdvTno: '5678',
-      cntrCstNo: '00000',
+      cntrCstKnm: '김계약',
+      lrnnCstKnm: '',
+      cralLocaraTno: '',
+      mexnoEncr: '',
+      cralIdvTno: '',
+      cntrCstNo: '',
+      cntrNo: '',
+      cntrSn: '',
     },
   },
   {
@@ -148,19 +116,4 @@ const pops = [
     },
   },
 ];
-
-const popInfo = ref({});
-pops.forEach((pop) => {
-  popInfo.value[pop.pname] = {
-    prm: JSON.stringify(pop.param),
-    rtn: '',
-  };
-});
-
-async function openPop(pname) {
-  popInfo.value[pname].rtn = JSON.stringify(await modal({
-    component: pname,
-    componentProps: JSON.parse(popInfo.value[pname].prm),
-  }));
-}
 </script>
