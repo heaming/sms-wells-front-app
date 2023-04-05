2a188a33bbbe14e96bb56ccf22f34b1b4c724690
김대영 <gs.piit40@kyowon.co.kr>
2023-04-05 13:46:41 +0900

    fix: homecard added

--- src/routes/IndexPage.vue
+++ src/routes/IndexPage.vue
@@ -1,13 +1,48 @@
 <template>
-  <kw-page class="flex flex-center">
+  <kw-page
+    v-if="!showHomeCard"
+    class="flex flex-center"
+  >
+    <kw-btn
+      label="홈 카드 보기"
+      style="display: flex;"
+      @click="onClickShowHomeCard"
+    />
     <img
       src="~~@assets/images/home.png"
       width="1100"
       alt="KSS Wells"
     >
   </kw-page>
+  <div
+    v-else
+    class="row"
+    style="width: 100%;"
+  >
+    <web-dashboard-m
+      :imported-components="components"
+    />
+  </div>
 </template>
 
 <script setup>
-//
+import { WebDashboardM, consts } from 'kw-lib';
+
+const { push } = useRouter();
+const store = useStore();
+
+const homecards = import.meta.globEager('../modules/common/components/homecard/*.vue');
+const components = shallowRef(
+  Object.keys(homecards).map((key) => ({
+    key,
+    component: homecards[key].default,
+  })),
+);
+const showHomeCard = ref(false);
+
+function onClickShowHomeCard() {
+  push({ name: consts.ROUTE_HOME_NAME });
+  store.commit('app/setLeftExist', false);
+  showHomeCard.value = true;
+}
 </script>
