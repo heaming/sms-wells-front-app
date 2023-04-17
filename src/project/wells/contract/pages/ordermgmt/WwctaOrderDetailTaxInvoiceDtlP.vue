392f723f27c61c29b9f2323517f65322374125ac
MyoungBin Joo <gs.piit165@kyowon.co.kr>
2023-04-17 16:10:18 +0900

    feat: 주문상세페이지 내부 팝업_세금계산서 (W-SS-U-0092P04) 프런트 엔드 개발

---
+++ src/project/wells/contract/pages/ordermgmt/WwctaOrderDetailTaxInvoiceDtlP.vue
@@ -0,0 +1,260 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : SSU
+2. 프로그램 ID : WwctaOrderDetailTaxInvoiceDtlP - 주문상세페이지 내부 팝업_세금계산서
+3. 작성자 : gs.piit165
+4. 작성일 : 2023.04.17
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+- [W-SS-U-0092P04] - 세금계산서 발행 팝업
+****************************************************************************************************
+--->
+<template>
+  <h3>{{ $t('MSG_TXT_SELT_CNTR_PD') }}</h3>
+  <kw-action-top class="mt30">
+    <kw-btn
+      dense
+      secondary
+      :label="t('MSG_BTN_INQR')"
+      @click="onClickSearch"
+    />
+    <kw-btn
+      v-show="isReadonly"
+      dense
+      secondary
+      :label="t('MSG_BTN_CH')"
+      @click="onClickEdit"
+    />
+    <kw-btn
+      v-show="!isReadonly"
+      dense
+      secondary
+      :label="t('MSG_BTN_SAVE')"
+      @click="onClickSave"
+    />
+    <kw-btn
+      dense
+      secondary
+      :label="t('MSG_BTN_CANCEL')"
+      @click="onClickCancel"
+    />
+  </kw-action-top>
+  <kw-form
+    ref="frmMainRef"
+    :cols="2"
+    class="mt20"
+  >
+    <kw-form-row>
+      <!-- 발행구분 -->
+      <kw-form-item
+        :label="t('MSG_TXT_ISSUANCE_CLAR')"
+        required
+      >
+        <kw-option-group
+          v-model="fieldParams.txinvPblOjYn"
+          :model-value="fieldParams.txinvPblOjYn"
+          :label="t('MSG_TXT_ISSUANCE_CLAR')"
+          type="radio"
+          :options="[
+            {'codeName':t('MSG_TXT_PBL'), 'codeId':'Y'},
+            {'codeName':t('MSG_TXT_N_PBL'), 'codeId':'N'}
+          ]"
+          :disable="isReadonly"
+          rules="required"
+        />
+      </kw-form-item>
+      <!-- 발행유형 -->
+      <kw-form-item :label="t('MSG_TXT_TXINV_PBL_TP')">
+        <kw-radio :label="t('MSG_TXT_ELC')" />
+      </kw-form-item>
+    </kw-form-row>
+
+    <kw-form-row>
+      <!-- 사업자등록번호 -->
+      <kw-form-item :label="t('MSG_TXT_CRNO')">
+        <p>{{ fieldParams.bzrnoFormat }}</p>
+      </kw-form-item>
+      <!-- 법인명 -->
+      <kw-form-item :label="t('MSG_TXT_CRP_NM')">
+        <p> {{ fieldParams.dlpnrNm }}</p>
+      </kw-form-item>
+    </kw-form-row>
+
+    <kw-form-row>
+      <!-- 담당자명 -->
+      <kw-form-item
+        :label="t('MSG_TXT_PIC_NM')"
+        required
+      >
+        <kw-input
+          v-model="fieldParams.dlpnrPsicNm"
+          maxlength="500"
+          :readonly="isReadonly"
+          :label="t('MSG_TXT_PIC_NM')"
+          rules="required"
+        />
+      </kw-form-item>
+      <!-- 전화번호 -->
+      <kw-form-item
+        :label="t('MSG_TXT_TEL_NO')"
+        required
+      >
+        <kw-input
+          v-model="fieldParams.telNo"
+          v-model:tel-no0="fieldParams.cralLocaraTno"
+          v-model:tel-no1="fieldParams.mexno"
+          v-model:tel-no2="fieldParams.cralIdvTno"
+          mask="telephone"
+          rules="required|telephone"
+          :readonly="isReadonly"
+          :label="t('MSG_TXT_TEL_NO')"
+        />
+      </kw-form-item>
+    </kw-form-row>
+
+    <kw-form-row>
+      <!-- 전자메일 -->
+      <kw-form-item
+        :label="t('MSG_TXT_EMAIL')"
+        required
+      >
+        <zwcm-email-address
+          v-model="fieldParams.emadr"
+          :readonly="isReadonly"
+          :label="t('MSG_TXT_EMAIL')"
+          :required="true"
+          rules="required"
+        />
+      </kw-form-item>
+      <!-- 발행일자 -->
+      <kw-form-item
+        :label="t('MSG_TXT_PBL_DT')"
+        required
+      >
+        <p class="ml8">
+          <kw-option-group
+            v-model="fieldParams.txinvPblD"
+            :model-value="fieldParams.txinvPblD"
+            :label="t('MSG_TXT_PBL_DT')"
+            rules="required"
+            type="radio"
+            :options="[
+              {'codeName':t('MSG_TXT_DAY', [15]), 'codeId':'15'},
+              {'codeName':t('MSG_TXT_DAY', [20]), 'codeId':'20'},
+              {'codeName':t('MSG_TXT_DAY', [25]), 'codeId':'25'},
+              {'codeName':t('MSG_TXT_LST_DAYS'), 'codeId':'99'}
+            ]"
+            :disable="isReadonly"
+          />
+        </p>
+      </kw-form-item>
+    </kw-form-row>
+  </kw-form>
+</template>
+
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { useDataService, getComponentType, useGlobal } from 'kw-lib';
+import { isEmpty, cloneDeep } from 'lodash-es';
+import ZwcmEmailAddress from '~common/components/ZwcmEmailAddress.vue';
+
+const { confirm, alert } = useGlobal();
+const { t } = useI18n();
+const dataService = useDataService();
+const isReadonly = ref(true);
+const frmMainRef = ref(getComponentType('KwForm'));
+
+let cachedParams;
+
+const props = defineProps({
+  cntrNo: { type: String, required: true, default: '' }, // 계약번호
+  cntrSn: { type: String, required: false, default: '' }, // 계약일련번호
+});
+
+const orgCntrNo = ref(''); /* 변경전 계약번호 */
+const orgCntrSn = ref(''); /* 변경전 계약일련번호 */
+
+const fieldParams = ref({
+  bzrno: '', /* 사업자등록번호 */
+  cntrCnfmDtm: '', /* 계약확정일시 */
+  cralIdvTno: '', /* 휴대개별전화번호 */
+  cralLocaraTno: '', /* 휴대지역전화번호 */
+  dlpnrCd: '', /* 거래처 코드 */
+  dlpnrNm: '', /* 거래처명 */
+  dlpnrPsicNm: '', /* 거래처담당자명 */
+  dpTpCd: '', /* 입금유형코드 */
+  emadr: '', /* 이메일 주소 */
+  emadr1: '', /* @앞 메일주소 */
+  emadr2: '', /* @뒤 메일주소 */
+  mexno: '', /* 휴대전화국번호암호화 */
+  txinvPblD: '', /* 세금계산서발행일 */
+  txinvPblOjYn: '', /* 세금계산서발행여부 */
+  telNo: '', /* 전화번호 */
+  bzrnoFormat: '', /* 사업자등록번호 format */
+});
+
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+
+// fetchData: 조회
+async function fetchData() {
+  if (!await frmMainRef.value.confirmIfIsModified()) { return; }
+  const res = await dataService.get('/sms/wells/contract/contract-info/tax-Invoices', { params: { cntrNo: props.cntrNo, cntrSn: props.cntrSn } });
+  if (!isEmpty(res.data)) {
+    Object.assign(fieldParams.value, res.data);
+    fieldParams.value.cntrNo = props.cntrNo; // 계약번호
+    fieldParams.value.cntrSn = props.cntrSn; // 계약일련번호
+    fieldParams.value.telNo = `${fieldParams.value.cralLocaraTno}${fieldParams.value.mexno}${fieldParams.value.cralIdvTno}`; // 전화번호
+    fieldParams.value.bzrnoFormat = !isEmpty(res.data.bzrno) ? `${res.data.bzrno?.substring(0, 3)}-${res.data.bzrno?.substring(3, 5)}-${res.data.bzrno?.substring(5, 10)}` : '';
+  }
+}
+
+// onClickSearch: 조회버튼 클릭 시
+async function onClickSearch() {
+  fetchData();
+}
+
+// onClickEdit: 수정버튼 클릭 시
+async function onClickEdit() {
+  isReadonly.value = !isReadonly.value;
+}
+
+// onClickCancel: 취소버튼 클릭 시
+async function onClickCancel() {
+  if (!await frmMainRef.value.confirmIfIsModified()) { return; }
+  isReadonly.value = true;
+}
+
+// onClickSave: 저장버튼 클릭 시
+async function onClickSave() {
+  if (await frmMainRef.value.alertIfIsNotModified()) { return; }
+
+  if (!await frmMainRef.value.validate()) { return; }
+
+  if (!await confirm(t('MSG_ALT_WANT_SAVE'))) { return; }
+
+  cachedParams = cloneDeep(fieldParams.value);
+
+  await dataService.post('/sms/wells/contract/contract-info/tax-Invoices', cachedParams);
+  await alert(t('MSG_ALT_SAVE_DATA'));
+}
+
+// onActivated: Tab 변경시
+onActivated(() => {
+  // 부모창의 cntrNo랑 cntrSn이 바뀌지 않았으면,
+  if (orgCntrNo.value === props.cntrNo && orgCntrSn.value === props.cntrSn) {
+    // 리턴
+    return;
+  }
+
+  // 부모창의 cntrNo랑 cntrSn이 바꼈으면, 다시 조회한다.
+  orgCntrNo.value = props.cntrNo;
+  orgCntrSn.value = props.cntrSn;
+});
+</script>
