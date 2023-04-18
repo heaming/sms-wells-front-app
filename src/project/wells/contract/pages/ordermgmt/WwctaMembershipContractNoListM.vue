20d8d9f845abddb1583ab0b86d8b8d8594559579
jihoon.kim <gs.piit193@kyowon.co.kr>
2023-04-18 09:57:32 +0900

    feat: 주문상세조회/관리 팝업 (렌탈/멤버쉽)

--- src/project/wells/contract/pages/ordermgmt/WwctaMembershipContractNoListM.vue
+++ src/project/wells/contract/pages/ordermgmt/WwctaMembershipContractNoListM.vue
@@ -424,7 +424,7 @@ const initGridMembershipContractNoList = defineGrid((data, view) => {
     if (['cntrDtlNo'].includes(column)) { // 계약상세(윈도우팝업)
       await modal({ component: 'WwctaOrderDetailP', componentProps: { cntrNo: paramCntrNo, cntrSn: paramCntrSn } });
     } else if (['ordrInfoView'].includes(column)) { // 멤버쉽 주문정보 상세
-      await modal({ component: 'WwctaOrderRentalDtlP', componentProps: { cntrNo: paramCntrNo, cntrSn: paramCntrSn } });
+      await modal({ component: 'WwctaOrderMembershipDtlP', componentProps: { cntrNo: paramCntrNo, cntrSn: paramCntrSn } });
     }
   };
 });
