f6ee405581166443fc8f383f8caf60e498c5a6d9
gs.nidhi.d <gs.nidhi.d@kyowon.co.kr>
2023-04-27 16:17:53 +0530

    feat: message resource registered & added

--- src/project/wells/contract/pages/changeorder/WwctbOrderBulkChangeMgtMSinlePayment.vue
+++ src/project/wells/contract/pages/changeorder/WwctbOrderBulkChangeMgtMSinlePayment.vue
@@ -1,21 +1,32 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : CTB
+2. 프로그램 ID : WwctbOrderBulkChangeMgtMSinlePayment - 일시불 일괄변경 관리
+3. 작성자 : gs.nidhi.d
+4. 작성일 : 2023.04.27
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+- 일시불 일괄변경 관리
+****************************************************************************************************
+--->
 <template>
   <kw-page>
-    <template #header>
-      <kw-page-header :options="['홈', '판매', '계약변경관리', '주문일괄변경 관리']" />
-    </template>
     <!-- To.개발 kw-tab-panel안에 kw-search로 시작하는 경우 kw-tabs에 .form-border 제거 / 그 외 추가 -->
     <kw-tabs model-value="3">
       <kw-tab
         name="1"
-        label="렌탈"
+        :label="$t('MSG_TXT_RENTAL')"
       />
       <kw-tab
         name="2"
-        label="맴버십"
+        :label="$t('MSG_TXT_MEM')"
       />
       <kw-tab
         name="3"
-        label="일시불"
+        :label="$t('MSG_TXT_SNGL_PMNT')"
       />
     </kw-tabs>
     <kw-tab-panels model-value="3">
@@ -25,10 +36,10 @@
           one-row
         >
           <kw-search-row>
-            <kw-search-item label="주문번호">
+            <kw-search-item :label="$t('MSG_TXT_ORD_NO')">
               <kw-input />
             </kw-search-item>
-            <kw-search-item label="반영일">
+            <kw-search-item :label="$t('MSG_TXT_RFLCTN_DT')">
               <kw-date-picker />
             </kw-search-item>
           </kw-search-row>
@@ -38,18 +49,19 @@
           <kw-action-top>
             <template #left>
               <kw-paging-info :total-count="156" />
-              <span class="ml8">(단위: 건)</span>
+              <span class="ml8">{{ t('MSG_TXT_UNIT_CASES') }}</span>
             </template>
             <kw-btn
-              label="일괄변경 등록"
+              :label="$t('MSG_TXT_BTCH_CHNG_REG')"
               primary
               dense
             />
           </kw-action-top>
           <kw-grid
-            ref="grdRef"
+            ref="grdMainRef"
+            name="grdMain"
             :visible-rows="10"
-            @init="initGrid"
+            @init="initGrdMain"
           />
         </div>
       </kw-tab-panel>
@@ -57,7 +69,17 @@
   </kw-page>
 </template>
 <script setup>
-function initGrid(data, view) {
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { defineGrid } from 'kw-lib';
+
+const { t } = useI18n();
+
+// -------------------------------------------------------------------------------------------------
+// Initialize Grid
+// -------------------------------------------------------------------------------------------------
+const initGrdMain = defineGrid((data, view) => {
   const fields = [
     { fieldName: 'col1' },
     { fieldName: 'col2' },
@@ -94,38 +116,38 @@ function initGrid(data, view) {
   ];
 
   const columns = [
-    { fieldName: 'col1', header: '주문번호', width: '152', styleName: 'text-center' },
-    { fieldName: 'col2', header: '고객명', width: '117', styleName: 'text-center' },
-    { fieldName: 'col3', header: '수납코드', width: '117', styleName: 'text-center' },
-    { fieldName: 'col4', header: '상품코드', width: '117', styleName: 'text-center' },
-    { fieldName: 'col5', header: '판매자', width: '117', styleName: 'text-center' },
-    { fieldName: 'col6', header: '수납코드', width: '117', styleName: 'text-center' },
-    { fieldName: 'col7', header: '상품명', width: '264' },
-    { fieldName: 'col8', header: '접수일', width: '117', styleName: 'text-center' },
-    { fieldName: 'col9', header: '설치일', width: '117', styleName: 'text-center' },
-    { fieldName: 'col10', header: '취소일', width: '117', styleName: 'text-center' },
-    { fieldName: 'col11', header: '보상일', width: '117', styleName: 'text-center' },
-    { fieldName: 'col12', header: '예정일', width: '117', styleName: 'text-center' },
-    { fieldName: 'col13', header: '실적취소일', width: '117', styleName: 'text-center' },
-    { fieldName: 'col14', header: 'KIWI철거일', width: '117', styleName: 'text-center' },
-    { fieldName: 'col15', header: '컨택명', width: '264' },
-    { fieldName: 'col16', header: '예약여부', width: '117', styleName: 'text-center' },
-    { fieldName: 'col17', header: '인정건수', width: '117', styleName: 'text-right' },
-    { fieldName: 'col18', header: '인정금액(원)', width: '117', styleName: 'text-right' },
-    { fieldName: 'col19', header: '인정율(%)', width: '117', styleName: 'text-right' },
-    { fieldName: 'col20', header: '기준수수료(원)', width: '131', styleName: 'text-right' },
-    { fieldName: 'col21', header: '정액여부', width: '117', styleName: 'text-center' },
-    { fieldName: 'col22', header: '할부이체', width: '117', styleName: 'text-right' },
-    { fieldName: 'col23', header: '할부실적', width: '117', styleName: 'text-right' },
-    { fieldName: 'col24', header: '멤버십이체', width: '117', styleName: 'text-right' },
-    { fieldName: 'col25', header: '멤버십원장', width: '117', styleName: 'text-center' },
-    { fieldName: 'col26', header: '무상멤버십', width: '117', styleName: 'text-right' },
-    { fieldName: 'col27', header: '무상A/S', width: '117', styleName: 'text-right' },
-    { fieldName: 'col28', header: '행사코드', width: '117', styleName: 'text-center' },
-    { fieldName: 'col29', header: '등록일', width: '117', styleName: 'text-center' },
-    { fieldName: 'col30', header: '등록자', width: '117', styleName: 'text-center' },
-    { fieldName: 'col31', header: '최종수정일', width: '117', styleName: 'text-center' },
-    { fieldName: 'col32', header: '최종수정자', width: '117', styleName: 'text-center' },
+    { fieldName: 'col1', header: t('MSG_TXT_ORD_NO'), width: '152', styleName: 'text-center' },
+    { fieldName: 'col2', header: t('MSG_TXT_CST_NM'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col3', header: t('MSG_TXT_RVE_CD'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col4', header: t('MSG_TXT_PRDT_CODE'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col5', header: t('MSG_TXT_SELLER_PERSON'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col6', header: t('MSG_TXT_RVE_CD'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col7', header: t('MSG_TXT_PRDT_NM'), width: '264' },
+    { fieldName: 'col8', header: t('MSG_TXT_RCP_D'), width: '117', styleName: 'text-center', datetimeFormat: 'date' },
+    { fieldName: 'col9', header: t('MSG_TXT_INST_DT'), width: '117', styleName: 'text-center', datetimeFormat: 'date' },
+    { fieldName: 'col10', header: t('MSG_TXT_CAN_D'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col11', header: t('MSG_TXT_COMP_D'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col12', header: t('MSG_TXT_DUEDT'), width: '117', styleName: 'text-center', datetimeFormat: 'date' },
+    { fieldName: 'col13', header: t('MSG_TXT_PRFMC_CNCL_DT'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col14', header: `KIWI${t('MSG_TXT_REQD_D')}`, width: '117', styleName: 'text-center' },
+    { fieldName: 'col15', header: t('MSG_TXT_CNTCT_NM'), width: '264' },
+    { fieldName: 'col16', header: t('MSG_TXT_RSV_YN'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col17', header: t('MSG_TXT_PD_ACC_CNT'), width: '117', styleName: 'text-right' },
+    { fieldName: 'col18', header: t('MSG_TXT_RECOG_AMT_KRW'), width: '117', styleName: 'text-right' },
+    { fieldName: 'col19', header: `${t('MSG_TXT_RECOG_RT')}(%)`, width: '117', styleName: 'text-right' },
+    { fieldName: 'col20', header: `${t('MSG_TXT_PD_STD_FEE')}(${t('MSG_TXT_CUR_WON')})`, width: '131', styleName: 'text-right' },
+    { fieldName: 'col21', header: t('MSG_TXT_FXAM_YN'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col22', header: t('MSG_TXT_ISTM_FNT'), width: '117', styleName: 'text-right' },
+    { fieldName: 'col23', header: t('MSG_TXT_INSTLMNT_PYMNT'), width: '117', styleName: 'text-right' },
+    { fieldName: 'col24', header: t('MSG_TXT_MEMBRSHP_TRSFR'), width: '117', styleName: 'text-right' },
+    { fieldName: 'col25', header: t('MSG_TXT_MEMBRSHP_LGDR'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col26', header: t('MSG_TXT_FRISU_MSH'), width: '117', styleName: 'text-right' },
+    { fieldName: 'col27', header: t('MSG_TXT_FRISU_AS'), width: '117', styleName: 'text-right' },
+    { fieldName: 'col28', header: t('MSG_TXT_EV_CD'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col29', header: t('MSG_TXT_RGST_DT'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col30', header: t('MSG_TXT_FST_RGST_USR'), width: '117', styleName: 'text-center' },
+    { fieldName: 'col31', header: t('MSG_TXT_FNL_MDFC_D'), width: '117', styleName: 'text-center', datetimeFormat: 'date' },
+    { fieldName: 'col32', header: t('MSG_TXT_FNL_MDFC_USR'), width: '117', styleName: 'text-center' },
   ];
 
   data.setFields(fields);
@@ -138,7 +160,7 @@ function initGrid(data, view) {
     { col1: '2022-1234567', col2: '김고객', col3: '12345', col4: '1234', col5: '김판매', col6: '12345', col7: '웰스(KWW-5100)', col8: '2022-10-10', col9: '2020-10-01', col10: '-', col11: '-', col12: '2020-10-01', col13: '-', col14: '-', col15: '고객접촉OK! 컨택완료!', col16: 'Y', col17: '0', col18: '1,600,000', col19: '100', col20: '0', col21: 'N', col22: '1', col23: '2', col24: '50', col25: 'Y', col26: '50', col27: '50', col28: 'F', col29: '2022-10-21', col30: '김교원', col31: '2022-10-21', col32: '김교원' },
 
   ]);
-}
+});
 </script>
 <style scoped>
 </style>
