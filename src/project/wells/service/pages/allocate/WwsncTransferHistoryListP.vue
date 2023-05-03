3e9048b779c127e636fa29915a82a226dd6c7961
juno.cha <gs.piit134@kyowon.com>
2023-05-03 17:32:44 +0900

    feature : W-SV-U-0019P01 이관이력 조회 1차 commit~

---
+++ src/project/wells/service/pages/allocate/WwsncTransferHistoryListP.vue
@@ -0,0 +1,125 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : SNC
+2. 프로그램 ID : WwsncTransferHistoryListP - 이관이력 조회
+3. 작성자 : juno.cha
+4. 작성일 : 2023.05.03
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+- 이관이력 조회 화면
+****************************************************************************************************
+--->
+<template>
+  <kw-popup
+    size="xl"
+  >
+    <kw-action-top>
+      <template #left>
+        <kw-paging-info :total-count="10" />
+      </template>
+    </kw-action-top>
+    <kw-grid
+      ref="gridMainRef"
+      :visible-rows="10"
+      @init="initGrid"
+    />
+    <template #action>
+      <kw-btn
+        primary
+        label="확인"
+        @click="onClickCancel"
+      />
+    </template>
+  </kw-popup>
+</template>
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { defineGrid, useDataService, getComponentType, useModal } from 'kw-lib';
+import { onMounted } from 'vue';
+
+const { t } = useI18n();
+const { cancel: onClickCancel } = useModal();
+const dataService = useDataService();
+
+/*
+ *  Parent Parameter를 가져오기 위한 변수 선언.
+ */
+const props = defineProps({
+  cstSvAsnNo: {
+    type: String,
+    required: true,
+  },
+});
+
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const gridMainRef = ref(getComponentType('KwGrid'));
+/*
+ * 이관이력 조회
+ */
+async function getTransferHistories() {
+  const res = await dataService.get('/sms/wells/service/transfer-histories', { params: { cstSvAsnNo: props.cstSvAsnNo } });
+  const view = gridMainRef.value.getView();
+  view.getDataSource().setRows(res.data);
+  view.resetCurrent();
+  // await dataService.get('/sms/wells/service/transfer-histories', { params: { cstSvAsnNo: props.cstSvAsnNo } });
+}
+
+onMounted(async () => {
+  await getTransferHistories();
+});
+
+// -------------------------------------------------------------------------------------------------
+// Initialize Grid
+// -------------------------------------------------------------------------------------------------
+const initGrid = defineGrid((data, view) => {
+  const fields = [
+    { fieldName: 'fnlMdfcDtm' },
+    { fieldName: 'asnTfDvCd' },
+    { fieldName: 'bfOgNm' },
+    { fieldName: 'bfCnfmPsicPrtnrNo' },
+    { fieldName: 'bfPrtnrKnm' },
+    { fieldName: 'afOgNm' },
+    { fieldName: 'afCnfmPsicPrtnrNo' },
+    { fieldName: 'afPrtnrKnm' },
+  ];
+
+  const columns = [
+    {
+      fieldName: 'fnlMdfcDtm', header: t('이관일시'), width: '200', styleName: 'text-center' },
+    { fieldName: 'asnTfDvCd', header: t('이관사유'), width: '150', styleName: 'text-center' },
+    { fieldName: 'bfOgNm', header: t('소속'), width: '150', styleName: 'text-left' },
+    { fieldName: 'bfCnfmPsicPrtnrNo', header: t('사번'), width: '100', styleName: 'text-right' },
+    { fieldName: 'bfPrtnrKnm', header: t('성명'), width: '100', styleName: 'text-left' },
+    { fieldName: 'afOgNm', header: t('소속'), width: '150', styleName: 'text-left' },
+    { fieldName: 'afCnfmPsicPrtnrNo', header: t('사번'), width: '100', styleName: 'text-right' },
+    { fieldName: 'afPrtnrKnm', header: t('성명'), width: '100', styleName: 'text-left' },
+
+  ];
+
+  data.setFields(fields);
+  view.setColumns(columns);
+
+  view.checkBar.visible = false;
+  view.rowIndicator.visible = true;
+  view.setColumnLayout([
+    'fnlMdfcDtm', 'asnTfDvCd',
+    {
+      header: t('이관 전 담당자'),
+      direction: 'horizontal',
+      items: ['bfOgNm', 'bfCnfmPsicPrtnrNo', 'bfPrtnrKnm'],
+    },
+    {
+      header: t('이관 전 담당자'),
+      direction: 'horizontal',
+      items: ['afOgNm', 'afCnfmPsicPrtnrNo', 'afPrtnrKnm'],
+    },
+  ]);
+});
+</script>
