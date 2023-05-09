6ca58aee6271cb03534624ed5e2480152fea6d13
김대영 <gs.piit40@kyowon.co.kr>
2023-05-09 14:10:39 +0900

    fix: vite build 변경 (변경된 파일만 hash 변경되도록)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'kw-lib/config/vite';
+import { createHash } from 'crypto';
 
 export default defineConfig({
   openVisualizer: false,
   sourcemap: false,
+  rollupOptions: {
+    output: {
+      entryFileNames: 'assets/[name].js',
+      chunkFileNames: (chunkInfo) => {
+        const hash = createHash('md5')
+          .update(Object.values(chunkInfo.modules).map((m) => m.code).join())
+          .digest('hex')
+          .substr(0, 6);
+        return `assets/[name].${hash}.js`;
+      },
+    },
+  },
 });
