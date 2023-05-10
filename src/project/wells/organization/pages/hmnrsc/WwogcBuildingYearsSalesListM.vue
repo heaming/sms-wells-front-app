deacd1313fb8ae1e87283e63388066cc92358588
Rahul Nirania <gs.rahul.n@kyowon.co.kr>
2023-05-10 20:05:53 +0900

    User/gs.rahul.n/20230508

--- src/project/wells/organization/pages/hmnrsc/WwogcBuildingYearsSalesListM.vue
+++ src/project/wells/organization/pages/hmnrsc/WwogcBuildingYearsSalesListM.vue
@@ -1,14 +1,23 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : OGC
+2. 프로그램 ID : WwogcBuildingYearsSalesListM - 사업장 단위별 년간영업 현황
+3. 작성자 : gs.rahul.n
+4. 작성일 : 2023-05-08
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+- 사업장 단위별 년간영업 현황
+****************************************************************************************************
+--->
 <template>
   <kw-page>
-    <template #header>
-      <kw-page-header
-        :options="['홈','nav','nav','사업장 단위별 년간영업 현황']"
-      />
-    </template>
     <kw-search :cols="2">
       <kw-search-row>
         <kw-search-item
-          label="실적년도"
+          :label="$t('MSG_TXT_PERF_Y')"
           required
         >
           <kw-date-picker
@@ -18,7 +27,7 @@
         </kw-search-item>
 
         <kw-search-item
-          label="실적기준"
+          :label="$t('MSG_TXT_PERF_BASE')"
           required
         >
           <kw-option-group
@@ -32,7 +41,7 @@
 
       <kw-search-row>
         <kw-search-item
-          label="조직유형"
+          :label="$t('MSG_TXT_OG_TP')"
           required
         >
           <kw-select
@@ -42,7 +51,7 @@
           />
         </kw-search-item>
         <kw-search-item
-          label="조직레벨"
+          :label="$t('MSG_TXT_OG_LEVL')"
           required
         >
           <kw-select
@@ -62,7 +71,7 @@
       </kw-search-row>
       <kw-search-row>
         <kw-search-item
-          label="빌딩"
+          :label="$t('MSG_TXT_BUILDING')"
           required
         >
           <kw-select
@@ -76,27 +85,67 @@
     <div class="result-area">
       <kw-action-top>
         <template #left>
-          <kw-paging-info :total-count="7" />
+          <kw-paging-info
+            v-model:page-index="pageInfo.pageIndex"
+            v-model:page-size="pageInfo.pageSize"
+            :total-count="pageInfo.totalCount"
+            :page-size-options="codes.COD_PAGE_SIZE_OPTIONS"
+            @change="fetchData"
+          />
         </template>
         <kw-btn
           icon="download_on"
           dense
           secondary
-          label="엑셀 다운로드"
+          :label="$t('MSG_BTN_EXCEL_DOWN')"
+          :disable="pageInfo.totalCount === 0"
           @click="onClickExcelDownload"
         />
       </kw-action-top>
 
       <kw-grid
         :visible-rows="10"
-        @init="initGrid"
+        @init="initGrdMain"
       />
     </div>
   </kw-page>
 </template>
 <script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { codeUtil, defineGrid, getComponentType, gridUtil, useMeta } from 'kw-lib';
 
-function initGrid(data, view) {
+const { t } = useI18n();
+const { getConfig } = useMeta();
+const { currentRoute } = useRouter();
+
+const pageInfo = ref({
+  totalCount: 0,
+  pageIndex: 1,
+  pageSize: Number(getConfig('CFG_CMZ_DEFAULT_PAGE_SIZE')),
+});
+const codes = await codeUtil.getMultiCodes(
+  'COD_PAGE_SIZE_OPTIONS',
+);
+const grdMainRef = ref(getComponentType('KwGrid'));
+
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+
+async function onClickExcelDownload() {
+  const view = grdMainRef.value.getView();
+  await gridUtil.exportView(view, {
+    fileName: currentRoute.value.meta.menuName,
+    timePostfix: true,
+  });
+}
+
+// -------------------------------------------------------------------------------------------------
+// Initialize Grid
+// -------------------------------------------------------------------------------------------------
+const initGrdMain = defineGrid((data, view) => {
   const fields = [
     { fieldName: 'col1' },
     { fieldName: 'col2' },
@@ -117,22 +166,22 @@ function initGrid(data, view) {
   ];
 
   const columns = [
-    { fieldName: 'col1', header: '소속', width: '92', styleName: 'text-center' },
-    { fieldName: 'col2', header: '빌딩', width: '202', styleName: 'text-center' },
-    { fieldName: 'col3', header: '구분1', width: '106', styleName: 'text-center' },
-    { fieldName: 'col4', header: '구분2', width: '148', styleName: 'text-center' },
-    { fieldName: 'col5', header: '1월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col6', header: '2월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col7', header: '3월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col8', header: '4월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col9', header: '5월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col10', header: '6월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col11', header: '7월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col12', header: '8월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col13', header: '9월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col14', header: '10월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col15', header: '11월', width: '80', styleName: 'text-center' },
-    { fieldName: 'col16', header: '12월', width: '80', styleName: 'text-center' },
+    { fieldName: 'col1', header: t('MSG_TXT_BLG'), width: '92', styleName: 'text-center' },
+    { fieldName: 'col2', header: t('MSG_TXT_BUILDING'), width: '202', styleName: 'text-center' },
+    { fieldName: 'col3', header: `${t('MSG_TXT_DIV')}1`, width: '106', styleName: 'text-center' },
+    { fieldName: 'col4', header: `${t('MSG_TXT_DIV')}2`, width: '148', styleName: 'text-center' },
+    { fieldName: 'col5', header: `1${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col6', header: `2${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col7', header: `3${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col8', header: `4${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col9', header: `5${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col10', header: `6${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col11', header: `7${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col12', header: `8${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col13', header: `9${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col14', header: `10${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col15', header: `11${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
+    { fieldName: 'col16', header: `12${t('MSG_TXT_MON')}`, width: '80', styleName: 'text-center' },
   ];
 
   data.setFields(fields);
@@ -148,6 +197,6 @@ function initGrid(data, view) {
     { col1: '-', col2: '-', col3: '-', col4: '-', col5: '-', col6: '-', col7: '-', col8: '-', col9: '-', col10: '-', col11: '-', col12: '-', col13: '-', col14: '-', col15: '-', col16: '-' },
 
   ]);
-}
+});
 
 </script>
