960725983833d6dc46c1adf30177fa56a209d91e
Min.Je <gs.piit191@kyowon.com>
2023-05-10 15:30:51 +0900

    feat: 수수료생성팝업 일괄생성중

---
+++ src/project/wells/fee/pages/calculation/WwfebOgFeeMlannerRegP.vue
@@ -0,0 +1,121 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : FEB
+2. 프로그램 ID : WwfebOgFeeMlannerRegP - M조직 수수료 생성관리 -> 수수료 생성
+3. 작성자 : mj
+4. 작성일 : 2023.04.17
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+- ? 수수료 수수료 관리 -> 수수료 생성 버튼클릭시 나오는 팝업이다.
+// 수수료 생성 버튼
+async function onClickCreate() {
+  // 조회후 진행
+  const { result: isChanged } = await modal({
+    component: 'WwfebOgFeeMlannerRegP',
+    componentProps: {
+      perfYm: searchParams.value.perfYm
+    },
+  });
+  if (isChanged) {
+    fetchData();
+  }
+}
+****************************************************************************************************
+--->
+<template>
+  <kw-popup
+    ref="popupRef"
+    size="sm"
+  >
+    <kw-form :cols="1">
+      <kw-form-row>
+        <kw-form-item
+          :label="$t('MSG_TXT_PERF_YM')"
+          required
+        >
+          <kw-date-picker
+            v-model="regData.perfYm"
+            rules="required"
+            type="month"
+            :label="$t('MSG_TXT_PERF_YM')"
+          />
+        </kw-form-item>
+      </kw-form-row>
+      <kw-form-row>
+        <kw-form-item
+          :label="$t('MSG_TXT_RSB_TP')"
+          required
+        >
+          <kw-select
+            v-model="regData.feeCalcUnitTpCd"
+            :options="codes.FEE_CALC_UNIT_TP_CD.filter((v) => ['201', '202'].includes(v.codeId))"
+            rules="required"
+            first-option="select"
+            :label="$t('MSG_TXT_RSB_TP')"
+          />
+        </kw-form-item>
+      </kw-form-row>
+    </kw-form>
+    <template #action>
+      <kw-btn
+        :label="$t('MSG_BTN_CANCEL')"
+        negative
+        @click="onClickCancel"
+      />
+      <kw-btn
+        :label="$t('MSG_BTN_CNTN_CREATE')"
+        primary
+        @click="onClickCreate"
+      />
+    </template>
+  </kw-popup>
+</template>
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { useModal, useDataService, useGlobal, codeUtil } from 'kw-lib';
+
+const { cancel, ok } = useModal();
+const { notify } = useGlobal();
+
+const { t } = useI18n();
+const props = defineProps({
+  perfYm: {
+    type: String,
+    default: '',
+  },
+});
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const codes = await codeUtil.getMultiCodes(
+  'FEE_CALC_UNIT_TP_CD',
+);
+const popupRef = ref();
+const dataService = useDataService();
+const regData = ref({
+  feeCalcUnitTpCd: '201',
+  feeTcntDvCd: '02', // 2차수
+  perfYm: props.perfYm,
+});
+// 취소
+async function onClickCancel() {
+  cancel();
+}
+// 생성
+async function onClickCreate() {
+  if (!await popupRef.value.validate()) { return; }
+
+  await dataService.post(`/sms/common/fee/fee-standards/calculate/${regData.value.perfYm}-${regData.value.feeTcntDvCd}-${regData.value.feeCalcUnitTpCd}`);
+  notify(t('MSG_ALT_CRT_FSH')); // 생성되었습니다.
+  ok(true);
+}
+// -------------------------------------------------------------------------------------------------
+// Initialize Grid
+// -------------------------------------------------------------------------------------------------
+
+</script>
