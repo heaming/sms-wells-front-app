6d8f9d923a7e6d8e4ee5ba4954491acca827214a
gs.piit129 <gs.piit129@kyowon.co.kr>
2023-05-12 18:21:55 +0900

    feat : 외주상품 A/S 관리 화면 및 연결화면 추가

---
+++ src/project/wells/service/pages/allocate/WwsncOutsourcingpdAsReceiptBiztalkP.vue
@@ -0,0 +1,119 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : SNC (배정관리)
+2. 프로그램 ID : WwsncOutsourcingpdAsReceiptBiztalkP - 외주상품 A/S 접수처 알림톡 발송
+3. 작성자 : yeonghwa.cheon
+4. 작성일 : 2023.05.09
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+- 제품 AS할 수 있는 외주 업체의 정보를 알림톡으로 발송하는 화면이다.
+****************************************************************************************************
+--->
+<template>
+  <kw-popup
+    size="xl"
+  >
+    <kw-form
+      ref="frmMainRef"
+      :cols="2"
+    >
+      <kw-form-row>
+        <kw-form-item :label="$t('MSG_TXT_DSPH_NO')">
+          <kw-input
+            readonly
+            model-value="1588-4113"
+          />
+        </kw-form-item>
+        <kw-form-item :label="$t('MSG_TXT_RECP_NO')">
+          <kw-input
+            v-model="biztalkParams.cstTno"
+            :label="$t('MSG_TXT_RECP_NO')"
+            rules="required|telephone"
+            mask="telephone"
+          />
+        </kw-form-item>
+      </kw-form-row>
+      <kw-form-row>
+        <kw-form-item
+          :colspan="2"
+          :label="$t('MSG_TXT_BIZTALK_SEND_CN')"
+        >
+          <kw-input
+            v-model="templateCn"
+            type="textarea"
+            :rows="7"
+            readonly
+          />
+        </kw-form-item>
+      </kw-form-row>
+    </kw-form>
+    <template #action>
+      <kw-btn
+        primary
+        :label="$t('MSG_BTN_SEND')"
+        @click="onClicBiztalkSend"
+      />
+    </template>
+  </kw-popup>
+</template>
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import {
+  // codeUtil,
+  // defineGrid,
+  // getComponentType,
+  useDataService,
+  // useMeta,
+  // gridUtil,
+  useGlobal,
+  useModal,
+} from 'kw-lib';
+// import { split } from 'lodash-es';
+
+const {
+  // notify,
+  alert,
+  confirm,
+} = useGlobal();
+const { t } = useI18n();
+const dataService = useDataService();
+const { ok } = useModal();
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const frmMainRef = ref();
+const props = defineProps({
+  cnrNm: {
+    type: String,
+    default: '신일전자',
+  },
+  cnrTno: {
+    type: String,
+    default: '02-333-5555',
+  },
+});
+
+const biztalkParams = ref({
+  cnrNm: props.cnrNm,
+  cnrTno: props.cnrTno,
+  cstTno: '',
+});
+
+const templateCn = (await dataService.post('/sflex/common/common/templates/TMP_SNC_WELLS17952/previews', biztalkParams.value)).data;
+
+async function onClicBiztalkSend() {
+  if (!await frmMainRef.value.validate()) { return; }
+  if (!await confirm(t('MSG_ALT_BIZTALK_CONFIRM'))) { return; }
+
+  const res = await dataService.post('/sms/wells/service/outsourcedpd-as-receipts/biztalk', biztalkParams.value);
+  if (res.data.processCount === 1) {
+    await alert(t('MSG_ALT_SEND_SUCCESS'));
+    ok();
+  }
+}
+</script>
