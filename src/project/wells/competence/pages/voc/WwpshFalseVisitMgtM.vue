bcb5195d61c90e9529bac2ca69ebf29a4a563c49
gs.piit225 <gs.piit225@kyowon.co.kr>
2023-05-30 09:14:53 +0900

    feat: 역량/총무 > 허위방문관리 신규메뉴 추가

---
+++ src/project/wells/competence/pages/voc/WwpshFalseVisitMgtM.vue
@@ -0,0 +1,367 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : CTB
+2. 프로그램 ID : WwpshFalseVisitMgtM - 허위방문 관리
+3. 작성자 : gs.piit225
+4. 작성일 : 2023.05.25
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+- 허위방문 관리
+****************************************************************************************************
+--->
+<template>
+  <kw-page>
+    <kw-search
+      one-row
+      :cols="2"
+      @search="onClickSearch"
+      @reset="onClickReset"
+    >
+      <kw-search-row>
+        <kw-search-item
+          :label="t('MSG_TXT_YEAR_OCCURNCE')"
+          :colspan="1"
+        >
+          <kw-date-picker
+            v-model="searchParams.ocYm"
+            :label="t('MSG_TXT_YEAR_OCCURNCE')"
+            type="month"
+          />
+        </kw-search-item>
+        <kw-search-item :label="$t('MSG_TXT_SEQUENCE_NUMBER')">
+          <kw-input
+            v-model="searchParams.prtnrNo"
+            clearable
+            readonly
+            icon="search"
+            :placeholder="$t('MSG_TXT_PRTNR_NO_FNM_IN')"
+            :on-click-icon="onClickSearchNo"
+            :regex="/^[A-Z가-힣ㄱ-ㅎ0-9]*$/i"
+          />
+
+          <kw-input
+            v-model="searchParams.prtnrKnm"
+            clearable
+            readonly
+            :placeholder="$t('MSG_TXT_EMPL_NM')"
+          />
+        </kw-search-item>
+      </kw-search-row>
+    </kw-search>
+    <div class="result-area">
+      <h3>{{ $t('MSG_TXT_SRCH_RSLT') }}</h3>
+      <kw-action-top>
+        <template #left>
+          <kw-paging-info
+            :page-index="pageInfo.pageIndex"
+            :page-size="pageInfo.pageSize"
+            :total-count="pageInfo.totalCount"
+            :page-size-options="codes.COD_PAGE_SIZE_OPTIONS"
+            @change="fetchData"
+          />
+        </template>
+        <kw-btn
+          :label="$t('MSG_BTN_DEL')"
+          dense
+          grid-action
+          @click="onClickDelete"
+        />
+        <kw-separator
+          vertical
+          inset
+          spaced
+        />
+        <kw-btn
+          v-permission:create
+          grid-action
+          :label="$t('MSG_BTN_ROW_ADD')"
+          @click="onClickAdd"
+        />
+        <kw-btn
+          :label="$t('MSG_BTN_SAVE')"
+          grid-action
+          @click="onClickSave"
+        />
+        <kw-separator
+          vertical
+          inset
+          spaced
+        />
+        <kw-btn
+          icon="excel"
+          dense
+          :label="$t('MSG_BTN_EXCEL_DOWN')"
+          @click="onClickExportView"
+        />
+      </kw-action-top>
+      <kw-grid
+        ref="grdMainRef"
+        name="grdMain"
+        :visible-rows="10"
+        @init="initGrid"
+      />
+      <kw-pagination
+        :page-index="pageInfo.pageIndex"
+        :page-size="pageInfo.pageSize"
+        :total-count="pageInfo.totalCount"
+        @change="fetchData"
+      />
+      <kw-separator />
+    </div>
+  </kw-page>
+</template>
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+import { getComponentType, gridUtil, codeUtil, useDataService, useGlobal, useMeta, notify } from 'kw-lib';
+import dayjs from 'dayjs';
+import { isEmpty, cloneDeep } from 'lodash-es';
+
+const { t } = useI18n();
+const dataService = useDataService();
+const { modal } = useGlobal();
+const { currentRoute } = useRouter();
+const now = dayjs();
+const { getUserInfo, getConfig } = useMeta();
+const userInfo = getUserInfo();
+const prtnrPopupValidateChk = ref(0);
+// 공통코드로딩
+const codes = await codeUtil.getMultiCodes(
+  'COD_PAGE_SIZE_OPTIONS',
+);
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const grdMainRef = ref(getComponentType('KwGrid'));
+let cachedParams;
+// 검색조건 Parameter
+const searchParams = ref({
+  ocYm: now.format('YYYYMM'),
+  prtnrNo: undefined,
+  prtnrKnm: undefined,
+});
+
+const pageInfo = ref({
+  totalCount: 0,
+  pageIndex: 1,
+  pageSize: Number(getConfig('CFG_CMZ_DEFAULT_PAGE_SIZE')),
+});
+
+// 번호 팝업 호출
+async function onClickSearchNo() {
+  const { result, payload } = await modal({
+    component: 'ZwogzPartnerListP',
+    componentProps: {
+      prtnrNo: searchParams.value.prtnrNo,
+      ogTpCd: userInfo.ogTpCd,
+    },
+  });
+
+  if (result) {
+    searchParams.value.prtnrNo = payload.prtnrNo;
+    searchParams.value.prtnrKnm = payload.prtnrKnm;
+  }
+}
+
+async function onClickAdd() {
+  const view = grdMainRef.value.getView();
+  let ocYm = '';
+  if (searchParams.value.ocYm === '') {
+    ocYm = now.format('YYYYMM');
+  } else {
+    ocYm = searchParams.value.ocYm;
+  }
+  gridUtil.insertRowAndFocus(view, 0, {
+    ocYm,
+    fsVstYm: now.format('YYYYMM'),
+    ogTpCd: userInfo.ogTpCd,
+  });
+}
+
+async function fetchData() {
+  const res = await dataService.get('/sms/wells/competence/voc-falsevisit/paging', { params: { ...cachedParams, ...pageInfo.value } });
+  const { list: products, pageInfo: pagingResult } = res.data;
+
+  pageInfo.value = pagingResult;
+
+  const view = grdMainRef.value.getView();
+  view.getDataSource().setRows(products);
+  view.resetCurrent();
+  view.rowIndicator.indexOffset = gridUtil.getPageIndexOffset(pageInfo);
+}
+
+async function onClickSearch() {
+  // 필요한 초기화 처리
+  cachedParams = cloneDeep(searchParams.value);
+
+  console.log(searchParams.value);
+  await fetchData();
+}
+
+async function onClickSave() {
+  const view = grdMainRef.value.getView();
+  const rows = gridUtil.getChangedRowValues(view, true);
+
+  if (await gridUtil.alertIfIsNotModified(view)) { return; }
+  if (!await gridUtil.validate(view)) { return; }
+
+  await dataService.post('/sms/wells/competence/voc-falsevisit', rows);
+
+  notify(t('MSG_ALT_SAVE_DATA'));
+  grdMainRef.value.getData().clearRows();
+
+  await fetchData();
+}
+
+async function onClickDelete() {
+  const view = grdMainRef.value.getView();
+  const deleteRows = await gridUtil.confirmDeleteCheckedRows(view);
+  if (deleteRows.length > 0) {
+    await dataService.delete('/sms/wells/competence/voc-falsevisit', { data: [...deleteRows] });
+    notify(t('MSG_ALT_DELETED'));
+    await fetchData();
+  }
+}
+async function onClickExportView() {
+  const view = grdMainRef.value.getView();
+
+  await gridUtil.exportView(view, {
+    fileName: currentRoute.value.meta.menuName,
+    timePostfix: true,
+  });
+}
+const onClickReset = async () => {
+  searchParams.value = {
+    ocYm: now.format('YYYYMM'),
+    prtnrNo: undefined,
+    prtnrKnm: undefined,
+  };
+};
+
+function initGrid(data, view) {
+  const fields = [
+    { fieldName: 'ocYm' },
+    { fieldName: 'rgstSn' },
+    { fieldName: 'prtnrNo' },
+    { fieldName: 'prtnrKnm' },
+    { fieldName: 'ogId' },
+    { fieldName: 'fsVstYm' },
+    { fieldName: 'cntrNo' },
+    { fieldName: 'cntrSn' },
+    { fieldName: 'hooPrtnrNo' },
+    { fieldName: 'hooPrtnrNm' },
+    { fieldName: 'ocRsonCn' },
+    { fieldName: 'dtaDlYn' },
+    { fieldName: 'cntrNoSn' },
+    { fieldName: 'cstKnm' },
+    { fieldName: 'ogTpCd' },
+
+  ];
+  const columns = [
+    { fieldName: 'ocYm', header: t('MSG_TXT_YEAR_OCCURNCE'), width: '100', styleName: 'text-center', editable: false },
+    { fieldName: 'prtnrNo',
+      rules: 'required',
+      header: { text: t('MSG_TXT_SEQUENCE_NUMBER') },
+      width: '110',
+      editor: { type: 'text' },
+      button: 'action',
+      styleName: 'text-left, rg-button-icon--search',
+    },
+    { fieldName: 'prtnrKnm', header: t('MSG_TXT_EMPL_NM'), width: '90', styleName: 'text-center', editable: false },
+
+    { fieldName: 'fsVstYm',
+      header: t('MSG_TXT_FASLE_YEAR'),
+      width: '110',
+      rules: 'required',
+      datetimeFormat: 'yyyy-MM',
+      editor: {
+        type: 'btdate',
+        btOptions: {
+          minViewMode: 1,
+        },
+      },
+    },
+    { fieldName: 'hooPrtnrNo', header: t('MSG_TXT_TOP_USER_NO'), width: '100', styleName: 'text-center', editable: false },
+    { fieldName: 'hooPrtnrNm', header: t('MSG_TXT_TOP_USER_NM'), width: '100', styleName: 'text-center', editable: false },
+    {
+      fieldName: 'cntrNoSn',
+      rules: 'required',
+      header: t('MSG_TXT_CNTR_DTL_NO'),
+      button: 'action',
+      width: '150',
+      styleName: 'text-left rg-button-icon--search',
+      displayCallback(grid, index) {
+        const { cntrNo, cntrSn } = grid.getValues(index.itemIndex);
+        if (!isEmpty(cntrNo)) {
+          return `${cntrNo}-${cntrSn}`;
+        }
+      },
+      styleCallback(grid, dataCell) {
+        return dataCell.item.rowState === 'created' ? { editable: false,
+          styleName: 'text-left rg-button-icon--search',
+          editor: {
+            type: 'number',
+          },
+        } : { styleName: 'text-left rg-button-icon--search', editable: false };
+      },
+
+    },
+    { fieldName: 'cstKnm', header: t('MSG_TXT_CST_NM'), width: '100', styleName: 'text-center', editable: false },
+    { fieldName: 'ocRsonCn', header: t('MSG_TXT_CLAIM_RSN'), width: '200', styleName: 'text-center' },
+  ];
+
+  data.setFields(fields);
+  view.setColumns(columns);
+  view.editOptions.editable = true;
+  view.checkBar.visible = true;
+  view.rowIndicator.visible = true;
+
+  view.onCellButtonClicked = async (g, { dataRow, column }) => {
+    const { prtnrNo, cntrCstKnm/* cntrNo, cntrSn */ } = gridUtil.getRowValue(g, dataRow);
+    if (column === 'prtnrNo') {
+      const { result, payload } = await modal({
+        component: 'ZwogzPartnerListP',
+        componentProps: {
+          prtnrNo,
+          ogTpCd: userInfo.ogTpCd,
+        },
+
+      });
+      if (result) {
+        if (prtnrPopupValidateChk.value - 1 < 0) {
+          prtnrPopupValidateChk.value = 0;
+        } else {
+          prtnrPopupValidateChk.value -= 1;
+        }
+        data.setValue(dataRow, 'prtnrNo', payload.prtnrNo);
+        data.setValue(dataRow, 'prtnrKnm', payload.prtnrKnm);
+        data.setValue(dataRow, 'hooPrtnrNo', payload.dgr3LevlDgPrtnrNo);
+        data.setValue(dataRow, 'hooPrtnrNm', payload.dgr3LevlDgPrtnrNm);
+      }
+    }
+    if (column === 'cntrNoSn') {
+      prtnrPopupValidateChk.value = 0;
+      const { result, payload } = await modal({
+        component: `${userInfo.tenantCd}wctaContractNumberListP`,
+
+        componentProps: {
+          cntrCstKnm,
+        },
+
+      });
+      if (result) {
+        data.setValue(dataRow, 'cntrNo', payload.cntrNo);
+        data.setValue(dataRow, 'cntrSn', payload.cntrSn);
+        data.setValue(dataRow, 'cstKnm', payload.cntrCstKnm);
+        data.setValue(dataRow, 'cntrNoSn', `${payload.cntrNo}-${payload.cntrSn}`);
+      }
+    }
+  };
+}
+onMounted(() => {
+  onClickSearch();
+});
+</script>
