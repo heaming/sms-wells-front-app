1486af26f68a10a54f93bfa7c45841132c9e8a48
송태성 <gs.piit58@kyowon.com>
2023-06-26 22:07:23 +0900

    chore : 요청자재보유현황 그리드 수정

--- src/project/wells/service/pages/stock/WwsnaRequestMaterialHaveListP.vue
+++ src/project/wells/service/pages/stock/WwsnaRequestMaterialHaveListP.vue
@@ -278,14 +278,14 @@ onMounted(async () => {
 
 const initGrdMain = defineGrid((data, view) => {
   const fields = [
-    { fieldName: 'ogNm' },
+    { fieldName: 'wareNm' },
     { fieldName: 'wareNo' },
     { fieldName: 'qty' },
 
   ];
 
   const columns = [
-    { fieldName: 'ogNm', header: t('MSG_TXT_WARE_NM'), width: '30', styleName: 'text-left' },
+    { fieldName: 'wareNm', header: t('MSG_TXT_WARE_NM'), width: '30', styleName: 'text-left' },
     { fieldName: 'wareNo', header: t('MSG_TXT_WARE_CD'), width: '30', styleName: 'text-center' },
     { fieldName: 'qty', header: t('MSG_TXT_STOC'), width: '20', styleName: 'text-right' },
 
