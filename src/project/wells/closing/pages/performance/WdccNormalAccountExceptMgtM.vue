adccbaf886a2f4d203f0cf3d446f3e6b7c0bb523
Kicheol Choi <gs.piit170@kyowon.com>
2023-07-03 10:05:05 +0900

    review: 정상계정 제외관리 - 수정
    * 메뉴명 수정

---
+++ src/project/wells/closing/pages/performance/WdccNormalAccountExceptMgtM.vue
@@ -0,0 +1,332 @@
+<!----
+****************************************************************************************************
+* 프로그램 개요
+****************************************************************************************************
+1. 모듈 : DCC
+2. 프로그램 ID : WdccNormalAccountExceptMgtM - 정상계정 제외관리 - W-CL-U-0067M01
+3. 작성자 : Kicheol Choi
+4. 작성일 : 2023.06.28
+****************************************************************************************************
+* 프로그램 설명
+****************************************************************************************************
+- 정상계정 제외관리
+****************************************************************************************************
+---->
+<template>
+  <kw-page>
+    <kw-search
+      :cols="3"
+      @search="onClickSearch"
+    >
+      <kw-search-row>
+        <kw-search-item :label="$t('MSG_TXT_INQR_BASE')">
+          <kw-option-group
+            v-model="searchParams.searchGubun"
+            type="radio"
+            :options="searchGubunCode"
+          />
+        </kw-search-item>
+
+        <kw-search-item
+          v-if="searchParams.searchGubun === '1'"
+          :label="$t('MSG_TXT_PRDT_CODE')"
+          :max-length="10"
+        >
+          <kw-input
+            v-model="searchParams.pdCd"
+          />
+        </kw-search-item>
+
+        <kw-search-item
+          v-else
+          :label="$t('MSG_TXT_CNTR_DTL_NO')"
+        >
+          <zctz-contract-detail-number
+            v-model:cntr-no="searchParams.cntrNo"
+            v-model:cntr-sn="searchParams.cntrSn"
+            :name="$t('MSG_TXT_CNTR_DTL_NO')"
+          />
+        </kw-search-item>
+
+        <kw-search-item
+          :label="$t('MSG_TXT_FST_RGST_DT')"
+        >
+          <kw-date-range-picker
+            v-model:from="searchParams.regDtFrom"
+            v-model:to="searchParams.regDtTo"
+            rules="date_range_months:1"
+          />
+        </kw-search-item>
+      </kw-search-row>
+    </kw-search>
+
+    <div class="result-area">
+      <kw-action-top>
+        <template #left>
+          <kw-paging-info
+            :total-count="totalCount"
+          />
+        </template>
+        <kw-btn
+          v-permission:delete
+          dense
+          grid-action
+          :label="$t('MSG_BTN_DEL')"
+          @click="onClickDelete"
+        />
+        <kw-separator
+          spaced
+          vertical
+          inset
+        />
+        <kw-btn
+          v-permission:create
+          dense
+          grid-action
+          :label="$t('MSG_BTN_ROW_ADD')"
+          @click="onClickAdd"
+        />
+        <kw-btn
+          v-permission:update
+          dense
+          grid-action
+          :label="$t('MSG_BTN_SAVE')"
+          @click="onClickSave"
+        />
+        <kw-separator
+          spaced
+          vertical
+          inset
+        />
+        <kw-btn
+          icon="upload_on"
+          dense
+          secondary
+          :label="$t('MSG_BTN_EXCEL_UP')"
+          @click="onClickExcelUpload"
+        />
+        <kw-btn
+          icon="download_on"
+          dense
+          secondary
+          :label="$t('MSG_TXT_EXCEL_DOWNLOAD')"
+          :disable="totalCount === 0"
+          @click="onClickExcelDownload"
+        />
+      </kw-action-top>
+
+      <kw-grid
+        v-if="searchParams.searchGubun === '1'"
+        ref="grdMainRef"
+        name="grdMain"
+        :visible-rows="10"
+        :total-count="totalCount"
+        @init="initGridMain"
+      />
+      <kw-grid
+        v-else
+        ref="grdMainRef"
+        name="grdCntr"
+        :visible-rows="10"
+        :total-count="totalCount"
+        @init="initGridCntr"
+      />
+    </div>
+  </kw-page>
+</template>
+
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { useDataService, codeUtil, getComponentType, useGlobal, gridUtil, defineGrid } from 'kw-lib';
+import dayjs from 'dayjs';
+import { cloneDeep } from 'lodash-es';
+import { getSearchGubunAccount } from '~sms-common/closing/utils/clUtil';
+import ZctzContractDetailNumber from '~sms-common/contract/components/ZctzContractDetailNumber.vue'; // 계약상세번호 팝업
+
+const dataService = useDataService();
+const { t } = useI18n();
+
+const { notify, modal } = useGlobal();
+const { currentRoute } = useRouter();
+
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const now = dayjs();
+const grdMainRef = ref(getComponentType('KwGrid'));
+const totalCount = ref(0);
+
+const codes = await codeUtil.getMultiCodes(
+  'PMOT_CD',
+  'SELL_TP_CD',
+  'SELL_TP_DTL_CD',
+);
+
+const searchGubunCode = await getSearchGubunAccount();
+
+let cachedParams;
+
+const searchParams = ref({
+  searchGubun: searchGubunCode[0].codeId,
+  pdCd: '',
+  cntrNo: '',
+  cntrSn: '',
+  regDtFrom: now.format('YYYYMM01'),
+  regDtTo: now.format('YYYYMMDD'),
+});
+
+async function fetchData() {
+  const apiParam = searchParams.value.searchGubun === '1' ? 'product' : 'contract';
+
+  const res = await dataService.get(`/sms/wells/closing/normal-account/${apiParam}/lists`, { params: { ...cachedParams } });
+  const lists = res.data;
+  totalCount.value = lists.length;
+
+  const view = grdMainRef.value.getView();
+  view.getDataSource().setRows(lists);
+}
+
+async function onClickSearch() {
+  cachedParams = cloneDeep(searchParams.value);
+  await fetchData();
+}
+
+async function onClickExcelDownload() {
+  const view = grdMainRef.value.getView();
+  await gridUtil.exportView(view, {
+    fileName: currentRoute.value.meta.menuName,
+    timePostfix: true,
+  });
+}
+
+async function onClickAdd() {
+  const view = grdMainRef.value.getView();
+
+  gridUtil.insertRowAndFocus(view, 0, {});
+}
+
+async function onClickSave() {
+  const view = grdMainRef.value.getView();
+  if (await gridUtil.alertIfIsNotModified(view)) { return; }
+  if (!await gridUtil.validate(view)) { return; }
+
+  const changedRows = gridUtil.getChangedRowValues(view);
+
+  await dataService.post('/sms/wells/closing/normal-account', changedRows);
+  await notify(t('MSG_ALT_SAVE_DATA'));
+
+  await fetchData();
+}
+
+async function onClickDelete() {
+  const view = grdMainRef.value.getView();
+  if (!await gridUtil.confirmIfIsModified(view)) { return; }
+
+  const deletedRows = await gridUtil.confirmDeleteCheckedRows(view);
+  if (deletedRows.length > 0) {
+    await dataService.delete('/sms/wells/closing/normal-account', { data: [...deletedRows] });
+    await fetchData();
+  }
+}
+
+async function onClickExcelUpload() {
+  const apiUrl = '/sms/wells/closing/normal-account/excel-upload';
+  const templateId = 'FOM_MSG_RESO_XLS_UP';
+  const {
+    result,
+  } = await modal({
+    component: 'ZwcmzExcelUploadP',
+    componentProps: { apiUrl, templateId },
+  });
+  if (result.status === 'S') {
+    notify(t('MSG_ALT_SAVE_DATA'));
+  }
+}
+
+const isChecked = ref(true);
+onMounted(async () => {
+  isChecked.value = true;
+});
+
+// -------------------------------------------------------------------------------------------------
+// Initialize Grid
+// -------------------------------------------------------------------------------------------------
+const initGridMain = defineGrid((data, view) => {
+  const columns = [
+    { fieldName: 'nomAccExcdId', visible: false },
+    { fieldName: 'sellTpCd', header: t('MSG_TXT_SEL_TYPE'), width: '120', styleName: 'text-center', options: codes.SELL_TP_CD, editor: { type: 'dropdown' } },
+    { fieldName: 'sellTpDtlCd', header: t('MSG_TXT_SELL_TP_DTL'), width: '120', styleName: 'text-center', options: codes.SELL_TP_DTL_CD, editor: { type: 'dropdown' } },
+    { fieldName: 'pdHclsfId', header: t('MSG_TXT_PD_HCLSF'), width: '120', styleName: 'text-center', editable: false },
+    { fieldName: 'pdMclsfId', header: t('MSG_TXT_PD_MCLSF'), width: '120', styleName: 'text-center', editable: false },
+    { fieldName: 'pdCd', header: t('MSG_TXT_PRDT_CODE'), width: '120', styleName: 'text-center' },
+    { fieldName: 'pdNm', header: t('MSG_TXT_PRDT_NM'), width: '120', editable: false },
+    { fieldName: 'pmotCd', header: t('MSG_TXT_PMOT_CD'), width: '120', styleName: 'text-center' },
+    { fieldName: 'pmotNm', header: t('MSG_TXT_PMOT_NM'), width: '120', options: codes.PMOT_CD, editable: false },
+    { fieldName: 'cntrStrtdt', header: `${t('MSG_TXT_CNTRCT_DT')}(${t('MSG_TXT_STRT')})`, width: '130', styleName: 'text-center', editor: { type: 'btdate' }, datetimeFormat: 'date' },
+    { fieldName: 'cntrEnddt', header: `${t('MSG_TXT_CNTRCT_DT')}(${t('MSG_TXT_SHUTDOWN')})`, width: '130', styleName: 'text-center', editor: { type: 'btdate' }, datetimeFormat: 'date' },
+    { fieldName: 'nomAccExcdRsonCn', header: t('MSG_TXT_EXCD_RSON'), width: '200' },
+    { fieldName: 'fstRgstDtm', header: t('MSG_TXT_FST_RGST_DT'), width: '120', styleName: 'text-center', datetimeFormat: 'date', editable: false },
+    { fieldName: 'fstRgstUsrId', header: t('MSG_TXT_RGST_ICHR_NO'), width: '120', styleName: 'text-center', editable: false },
+    { fieldName: 'fstUserNm', header: t('MSG_TXT_RGST_PSIC'), width: '120', styleName: 'text-center', editable: false },
+    { fieldName: 'fnlMdfcDtm', header: t('MSG_TXT_MDFC_DATE'), width: '120', styleName: 'text-center', datetimeFormat: 'date', editable: false },
+    { fieldName: 'fnlMdfcUsrId', header: t('MSG_TXT_MDFC_ICHR_NO'), width: '120', styleName: 'text-center', editable: false },
+    { fieldName: 'fnlUserNm', header: t('MSG_TXT_MDFC_PSIC'), width: '120', styleName: 'text-center', editable: false },
+  ];
+
+  const fields = columns.map(({ fieldName, dataType }) => (dataType ? { fieldName, dataType } : { fieldName }));
+  data.setFields(fields);
+  view.setColumns(columns);
+
+  view.checkBar.visible = true;
+  view.rowIndicator.visible = true;
+  view.editOptions.editable = true;
+});
+
+const initGridCntr = defineGrid((data, view) => {
+  const columns = [
+    { fieldName: 'nomAccExcdId', visible: false },
+    { fieldName: 'cntrNo', visible: false },
+    { fieldName: 'cntrSn', visible: false },
+    { fieldName: 'cntrDtl', header: t('MSG_TXT_CNTR_DTL'), width: '120', styleName: 'text-center rg-button-icon--search', button: 'action' },
+    { fieldName: 'cntrExcdRsonCn', header: t('MSG_TXT_CNTN'), width: '180' },
+    { fieldName: 'nomAccExcdRsonCn', header: t('MSG_TXT_EXCD_RSON'), width: '200' },
+    { fieldName: 'fstRgstDtm', header: t('MSG_TXT_FST_RGST_DT'), width: '120', styleName: 'text-center', datetimeFormat: 'date', editable: false },
+    { fieldName: 'fstRgstUsrId', header: t('MSG_TXT_RGST_ICHR_NO'), width: '120', styleName: 'text-center', editable: false },
+    { fieldName: 'fstUserNm', header: t('MSG_TXT_RGST_PSIC'), width: '120', styleName: 'text-center', editable: false },
+    { fieldName: 'fnlMdfcDtm', header: t('MSG_TXT_MDFC_DATE'), width: '120', styleName: 'text-center', datetimeFormat: 'date', editable: false },
+    { fieldName: 'fnlMdfcUsrId', header: t('MSG_TXT_MDFC_ICHR_NO'), width: '120', styleName: 'text-center', editable: false },
+    { fieldName: 'fnlUserNm', header: t('MSG_TXT_MDFC_PSIC'), width: '120', styleName: 'text-center', editable: false },
+  ];
+
+  const fields = columns.map(({ fieldName, dataType }) => (dataType ? { fieldName, dataType } : { fieldName }));
+  data.setFields(fields);
+  view.setColumns(columns);
+
+  view.checkBar.visible = true;
+  view.rowIndicator.visible = true;
+  view.editOptions.editable = true;
+
+  view.onCellButtonClicked = async (grid/* , { itemIndex } */) => {
+    grid.commit();
+    const current = view.getCurrent();
+    // const dataProvider = view.getDataSource();
+    // const cntrDtl = dataProvider.getValue(current.dataRow, 'cntrDtl');
+    const { result, payload } = await modal({
+      component: 'WwctaContractNumberListP',
+    });
+    if (result) {
+      if (result.length === 0) {
+        grid.setValue(current.itemIndex, 'cntrDtl', '');
+        grid.setValue(current.itemIndex, 'cntrNo', '');
+        grid.setValue(current.itemIndex, 'cntrSn', '');
+        return;
+      }
+      grid.setValue(current.itemIndex, 'cntrDtl', `${payload.cntrNo}-${payload.cntrSn}`);
+      grid.setValue(current.itemIndex, 'cntrNo', payload.cntrNo);
+      grid.setValue(current.itemIndex, 'cntrSn', payload.cntrSn);
+    }
+  };
+});
+</script>
