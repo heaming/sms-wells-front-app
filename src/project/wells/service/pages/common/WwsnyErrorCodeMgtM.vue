9101f3d1b45f1aecb7923e6dd219204de62d0105
37758 <hunee95@kyowon.co.kr>
2023-07-18 17:29:25 +0900

    feat: 상품별 에러코드 등록 및 관리 개발 완료

---
+++ src/project/wells/service/pages/common/WwsnyErrorCodeMgtM.vue
@@ -0,0 +1,289 @@
+<!----
+ ****************************************************************************************************
+ * 프로그램 개요
+ ****************************************************************************************************
+1. 모듈 : SNY
+2. 프로그램 ID : WwsnyErrorCodeMgtM(K-W-SV-U-0304M01) - 상품별 에러코드 관리
+3. 작성자 : jaehunlee
+4. 작성일 : 2023.07.03
+ ****************************************************************************************************
+ * 프로그램 설명
+ ****************************************************************************************************
+- 상품별 에러코드 관리 (http://localhost:3000/#/service/wwsny-error-code-mgt)
+ ****************************************************************************************************
+--->
+<template>
+  <kw-page ref="pageRef">
+    <!-- rev:230420 one-row 추가  -->
+    <kw-search
+      one-row
+      @search="onClickSearch"
+    >
+      <kw-search-row>
+        <kw-search-item :label="$t('MSG_TXT_PD_GRP')">
+          <kw-select
+            v-model="searchParams.pdGrpCd"
+            :label="$t('MSG_TXT_PD_GRP')"
+            :options="codes.PD_GRP_CD"
+            first-option="all"
+          />
+          <kw-select
+            v-model="searchParams.pdCd"
+            :options="productCode"
+            first-option="all"
+          />
+        </kw-search-item>
+      </kw-search-row>
+    </kw-search>
+    <div class="result-area">
+      <kw-action-top>
+        <template #left>
+          <kw-paging-info
+            v-model:page-index="pageInfo.pageIndex"
+            v-model:page-size="pageInfo.pageSize"
+            :page-size-options="codes.COD_PAGE_SIZE_OPTIONS"
+            :total-count="pageInfo.totalCount"
+            @change="fetchData"
+          />
+        </template>
+        <kw-btn
+          dense
+          grid-action
+          :label="$t('MSG_BTN_DEL')"
+          @click="onClickDelete"
+        />
+        <kw-separator
+          spaced
+          vertical
+          inset
+        />
+        <kw-btn
+          icon="print"
+          dense
+          secondary
+          :label="t('MSG_BTN_PRTG')"
+          @click="onClickPrintEl"
+        />
+        <kw-btn
+          icon="download_on"
+          dense
+          secondary
+          :label="$t('MSG_BTN_EXCEL_DOWN')"
+          :disable="pageInfo.totalCount === 0"
+          @click="onClickExcelDownload"
+        />
+        <kw-separator
+          spaced
+          vertical
+          inset
+        />
+        <kw-btn
+          dense
+          primary
+          :label="$t('MSG_TXT_ERR_RGST')"
+          @click="onClickErrorCodeRgst('','','','','','')"
+        />
+      </kw-action-top>
+      <kw-grid
+        ref="grdMainRef"
+        :page-size="pageInfo.pageSize"
+        :visible-rows="10"
+        @init="initGrdMain"
+      />
+      <kw-pagination
+        v-model:page-index="pageInfo.pageIndex"
+        v-model:page-size="pageInfo.pageSize"
+        :total-count="pageInfo.totalCount"
+        @change="fetchData"
+      />
+    </div>
+  </kw-page>
+</template>
+
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { codeUtil, useMeta, useDataService, getComponentType, defineGrid, gridUtil, useGlobal } from 'kw-lib';
+import { cloneDeep } from 'lodash-es';
+import smsCommon from '~sms-wells/service/composables/useSnCode';
+import { printElement } from '~common/utils/common';
+
+const { t } = useI18n();
+const { getPartMaster } = smsCommon();
+const { getConfig } = useMeta();
+const dataService = useDataService();
+const { currentRoute } = useRouter();
+const { modal, notify } = useGlobal();
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const grdMainRef = ref(getComponentType('KwGrid'));
+let cachedParams;
+const searchParams = ref({
+  pdGrpCd: '',
+  pdCd: '',
+});
+
+const pageInfo = ref({
+  totalCount: 0,
+  pageIndex: 1,
+  pageSize: Number(getConfig('CFG_CMZ_DEFAULT_PAGE_SIZE')),
+});
+
+const codes = await codeUtil.getMultiCodes(
+  'COD_PAGE_SIZE_OPTIONS',
+  'PD_GRP_CD',
+  'COD_YN',
+);
+
+const productCode = ref();
+watch(() => [searchParams.value.year, searchParams.value.pdGrpCd], async () => {
+  // productCode.value = await getMcbyCstSvOjIz(searchParams.value.year, searchParams.value.pdGrpCd);
+  const tempVal = await getPartMaster(undefined, searchParams.value.pdGrpCd);
+  productCode.value = tempVal.map((v) => ({ codeId: v.cd, codeName: v.codeName }));
+}, { immediate: true });
+
+async function fetchData() {
+  const res = await dataService.get('/sms/wells/service/error-code/paging', { params: { ...cachedParams, ...pageInfo.value } });
+  const { list: products, pageInfo: pagingResult } = res.data;
+  pageInfo.value = pagingResult;
+  const view = grdMainRef.value.getView();
+  view.getDataSource().setRows(products);
+  view.resetCurrent();
+}
+
+async function onClickSearch() {
+  pageInfo.value.pageIndex = 1;
+  cachedParams = cloneDeep(searchParams.value);
+  await fetchData();
+}
+
+async function onClickExcelDownload() {
+  const view = grdMainRef.value.getView();
+  const res = await dataService.get('/sms/wells/service/error-code/excel-download', { params: { ...cachedParams } });
+
+  await gridUtil.exportView(view, {
+    fileName: currentRoute.value.meta.menuName,
+    timePostfix: true,
+    exportData: res.data,
+  });
+}
+
+async function onClickDelete() {
+  const view = grdMainRef.value.getView();
+  const deletedRows = await gridUtil.confirmDeleteCheckedRows(view);
+  if (deletedRows.length > 0) {
+    const result = await dataService.delete('/sms/wells/service/error-code', { data: [...deletedRows] });
+    if (result.data.processCount > 0) {
+      notify(t('MSG_ALT_SAVE_COMP'));
+    } else {
+      notify(t('MSG_ALT_PROC_FAIL'));
+    }
+    await fetchData();
+  }
+}
+
+async function onClickErrorCodeRgst(pdCd, errCn, errDvCn, errCausCn, errImageUId, errImageDocId, flag) {
+  console.log(errImageUId);
+  console.log(errImageDocId);
+  const { result: isChanged } = await modal({
+    component: 'WwsnyErrorCodeRegP',
+    componentProps: { pdCd, errCn, errDvCn, errCausCn, errImageUId, errImageDocId, flag },
+  });
+
+  if (isChanged) {
+    notify(t('MSG_ALT_SAVE_DATA'));
+    await fetchData();
+  }
+}
+
+const pageRef = ref();
+async function onClickPrintEl() {
+  printElement(pageRef);
+}
+// -------------------------------------------------------------------------------------------------
+// Initialize Grid
+// -------------------------------------------------------------------------------------------------
+const initGrdMain = defineGrid((data, view) => {
+  const fields = [
+    { fieldName: 'pdCd' },
+    { fieldName: 'pdNm' },
+    { fieldName: 'errCn' },
+    { fieldName: 'errDvCn' },
+    { fieldName: 'errCausCn' },
+    { fieldName: 'errImageYn' },
+    { fieldName: 'errImageUId' },
+    { fieldName: 'errImageDocId' },
+  ];
+  const columns = [
+    { fieldName: 'pdNm',
+      header: t('MSG_TXT_ITM_NM'),
+      width: '120',
+      styleName: 'text-left',
+    },
+    { fieldName: 'errCn',
+      header: t('MSG_TXT_ERR_TP'),
+      width: '130',
+      styleName: 'text-left',
+    },
+    { fieldName: 'errDvCn',
+      header: t('MSG_TXT_ERR_CN'),
+      width: '130',
+      styleName: 'text-left',
+    },
+    { fieldName: 'errCausCn',
+      header: t('MSG_TXT_ERR_DTL'),
+      width: '200',
+      styleName: 'text-left',
+    },
+    { fieldName: 'errImageYn',
+      header: t('MSG_TXT_IMG'),
+      width: '100',
+      styleName: 'text-center',
+      options: codes.COD_YN,
+      renderer: {
+        type: 'button',
+        hideWhenEmpty: true,
+      },
+      displayCallback: () => t('MSG_TXT_IMG_BRWS'),
+    },
+  ];
+
+  const columnLayout = [
+    'pdNm',
+    'errCn',
+    'errDvCn',
+    'errCausCn',
+    'errImageYn',
+  ];
+
+  data.setFields(fields);
+  view.setColumns(columns);
+  view.setColumnLayout(columnLayout);
+  view.checkBar.visible = true;
+  view.rowIndicator.visible = true;
+
+  view.onCellItemClicked = async (g, { column, itemIndex }) => {
+    const { errImageUId } = g.getValues(itemIndex);
+    console.log(errImageUId);
+    if (column === 'errImageYn') {
+      await modal({
+        component: 'ZwcmzImagePreviewP',
+        componentProps: { files: [
+          { fileUid: errImageUId },
+        ] }, // fileUid만 주면 됨
+      });
+    }
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  view.onCellDblClicked = (grid, clickData) => {
+    const currentDataRow = view.getCurrent().dataRow;
+    const rowData = gridUtil.getRowValue(view, currentDataRow);
+
+    const { pdCd, errCn, errDvCn, errCausCn, errImageUId, errImageDocId } = rowData;
+    onClickErrorCodeRgst(pdCd, errCn, errDvCn, errCausCn, errImageUId, errImageDocId, 'U');
+  };
+});
+</script>
---
+++ src/project/wells/service/pages/common/WwsnyErrorCodeRegP.vue
@@ -0,0 +1,204 @@
+<!----
+ ****************************************************************************************************
+ * 프로그램 개요
+ ****************************************************************************************************
+1. 모듈 : SNY
+2. 프로그램 ID : WwsnyErrorCodeRegP(K-W-SV-U-0304P01) - 상품별 에러코드 등록
+3. 작성자 : jaehunlee
+4. 작성일 : 2023.07.03
+ ****************************************************************************************************
+ * 프로그램 설명
+ ****************************************************************************************************
+- 상품별 에러코드 관리 (http://localhost:3000/#/service/wwsny-error-code-reg)
+ ****************************************************************************************************
+--->
+<template>
+  <kw-popup
+    size="md"
+    :title="$t('MSG_TIT_PRD_ERR_RGST')"
+  >
+    <kw-form
+      ref="frmMainRef"
+      :cols="1"
+      :label-size="120"
+    >
+      <kw-form-row>
+        <kw-form-item
+          :label="$t('MSG_TXT_ITM_NM')"
+          required
+        >
+          <kw-select
+            v-model="errorCodeInfo.pdCd"
+            :options="productCode"
+            :readonly="hasProps()"
+          />
+        </kw-form-item>
+      </kw-form-row>
+      <kw-form-row>
+        <kw-form-item
+          :label="$t('MSG_TXT_ERR_TP')"
+          required
+        >
+          <kw-input
+            v-model="errorCodeInfo.errCn"
+            type="textarea"
+            :rows="3"
+          />
+        </kw-form-item>
+      </kw-form-row>
+      <kw-form-row>
+        <kw-form-item
+          :label="$t('MSG_TXT_ERR_CN')"
+          required
+        >
+          <kw-input
+            v-model="errorCodeInfo.errDvCn"
+            type="textarea"
+            :rows="3"
+          />
+        </kw-form-item>
+      </kw-form-row>
+      <kw-form-row>
+        <kw-form-item
+          :label="$t('MSG_TXT_ERR_DTL')"
+        >
+          <kw-input
+            v-model="errorCodeInfo.errCausCn"
+            type="textarea"
+            :rows="3"
+          />
+        </kw-form-item>
+      </kw-form-row>
+      <kw-form-row>
+        <kw-form-item
+          :label="$t('MSG_TXT_IMG')"
+        >
+          <zwcm-file-attacher
+            ref="attachFileRef"
+            v-model="attachFiles"
+            :attach-document-id="errorCodeInfo.errImageDocId"
+            attach-group-id="ATG_SNY_ERR_IMG"
+          />
+        </kw-form-item>
+      </kw-form-row>
+    </kw-form>
+    <template #action>
+      <kw-btn
+        negative
+        :label="$t('MSG_TXT_CANCEL')"
+      />
+      <kw-btn
+        primary
+        :label="$t('MSG_TXT_RGS')"
+        @click="onClickSave"
+      />
+    </template>
+  </kw-popup>
+</template>
+<script setup>
+// -------------------------------------------------------------------------------------------------
+// Import & Declaration
+// -------------------------------------------------------------------------------------------------
+import { getComponentType, useDataService, useGlobal, useModal } from 'kw-lib';
+import ZwcmFileAttacher from '~common/components/ZwcmFileAttacher.vue';
+import { isEmpty } from 'lodash-es';
+
+const dataService = useDataService();
+const { notify, confirm } = useGlobal();
+const { t } = useI18n();
+const { ok } = useModal();
+
+// -------------------------------------------------------------------------------------------------
+// Function & Event
+// -------------------------------------------------------------------------------------------------
+const frmMainRef = ref(getComponentType('KwForm'));
+const attachFiles = ref([]);
+
+const props = defineProps({
+  pdCd: {
+    type: String,
+    default: '',
+  },
+  errCn: {
+    type: String,
+    default: '',
+  },
+  errDvCn: {
+    type: String,
+    default: '',
+  },
+  errCausCn: {
+    type: String,
+    default: '',
+  },
+  errImageUId: {
+    type: String,
+    default: '',
+  },
+  errImageDocId: {
+    type: String,
+    default: '',
+  },
+  flag: {
+    type: String,
+    default: 'I',
+  },
+});
+
+const initialErrorCodeInfo = {
+  pdCd: '',
+  errCn: '',
+  errDvCn: '',
+  errCausCn: '',
+  errImageUId: '',
+  errImageDocId: '',
+  attachFiles: [],
+  flag: 'I',
+};
+
+const errorCodeInfo = ref(initialErrorCodeInfo);
+function hasProps() {
+  return !isEmpty(props.pdCd);
+}
+
+function resetErrorCodeInfo() {
+  errorCodeInfo.value.pdCd = props.pdCd;
+  errorCodeInfo.value.errCn = props.errCn;
+  errorCodeInfo.value.errDvCn = props.errDvCn;
+  errorCodeInfo.value.errCausCn = props.errCausCn;
+  errorCodeInfo.value.errImageUId = props.errImageUId;
+  errorCodeInfo.value.errImageDocId = props.errImageDocId;
+  errorCodeInfo.value.attachFiles = attachFiles.value;
+  errorCodeInfo.value.flag = props.flag;
+  // errorCodeInfo.value.attachFiles = attachFiles.value;
+}
+
+watch(() => errorCodeInfo.value.pdCd, async (val) => {
+  if (errorCodeInfo.value.pdCd !== val) {
+    errorCodeInfo.value.pdCd = val;
+  }
+});
+
+const productCode = ref([]);
+
+const prd = (await dataService.get('/sms/wells/service/installation-locations/products')).data;
+productCode.value = prd;
+
+onMounted(async () => {
+  if (hasProps()) {
+    resetErrorCodeInfo();
+    await frmMainRef.value.init();
+  }
+});
+
+async function onClickSave() {
+  if (!await confirm(t('MSG_ALT_WANT_SAVE'))) { return; }
+  errorCodeInfo.value.attachFiles = attachFiles.value;
+  const data = errorCodeInfo.value;
+  console.log(data);
+  await dataService.post('/sms/wells/service/error-code', data);
+  notify(t('MSG_ALT_SAVE_DATA'));
+
+  ok();
+}
+</script>
