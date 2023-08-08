5906fb21612d843ae132c10acec79def4be688b8
minkyu.bae <gs.piit150.kyowon.co.kr>
2023-08-08 10:18:35 +0900

    chore : 수수료 테스트 오류 수정

--- src/project/wells/fee/pages/calculation/WwfebOgFeePlannerListM.vue
+++ src/project/wells/fee/pages/calculation/WwfebOgFeePlannerListM.vue
@@ -213,7 +213,7 @@ const searchParams = ref({
 
 const approval = ref({
   gb: 'ngt002', /* formId를 식별하는 구분 */
-  empno: sessionUserInfo.userId, /* 결재자 사번 */
+  empno: sessionUserInfo.employeeIDNumber, /* 결재자 사번 */
   formId: '2023000036', /* P조직 품의결재 폼ID */
   appKey: '', /* 업무단에서 해당 결재를 확인할 KEY */
 });
