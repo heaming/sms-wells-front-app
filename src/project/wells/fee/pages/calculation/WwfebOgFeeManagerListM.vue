783cb1e8fed191ed66a8d033554e92c3500cde5b
minkyu.bae <gs.piit150.kyowon.co.kr>
2023-08-21 13:35:09 +0900

    fix : 수수료 오류사항 수정

--- src/project/wells/fee/pages/calculation/WwfebOgFeeManagerListM.vue
+++ src/project/wells/fee/pages/calculation/WwfebOgFeeManagerListM.vue
@@ -414,7 +414,7 @@ async function onClickRetry(feeSchdId, feeSchdLvCd, feeSchdLvStatCd) {
 async function onClickW201P(feeSchdId, feeSchdLvCd, feeSchdLvStatCd) {
   const { feeTcntDvCd, perfYm, rsbTpCd } = searchParams.value;
   const { codeName } = codes.FEE_TCNT_DV_CD.find((v) => v.codeId === searchParams.value.feeTcntDvCd);
-  if (searchParams.value.rsbTp === '') {
+  if (rsbTpCd === '') {
     await alert(t('MSG_ALT_SELECT_RSB_TP'));
   } else {
     const param = {
@@ -465,13 +465,14 @@ async function onClickW202P(feeSchdId, feeSchdLvCd, feeSchdLvStatCd) {
  *  Event - 기타지원 업로드 클릭 ※업무개발에서 별도개발
  */
 async function onClickW205P(feeSchdId, feeSchdLvCd, feeSchdLvStatCd) {
+  const { rsbTpCd } = searchParams.value;
   const { result: isUploadSuccess, payload } = await modal({
     component: 'ZwfezXlsUpP',
     componentProps: {
       formatId: 'FOM_FEZ_0023',
       baseYm: searchParams.value.perfYm,
       ogTpCd: 'W02',
-      type: searchParams.value.rsbTp,
+      type: rsbTpCd,
     },
   });
   if (isUploadSuccess) {
