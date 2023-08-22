0a10677b470016946bcc48587a5cee8c955d8ba1
gilyong.han <gs.piit128@kyowon.co.kr>
2023-08-22 12:00:41 +0900

    fix: styleCallback 재수정

--- src/project/wells/bond/pages/consultation/WwbncRentalResignExpectedMgtMExpected.vue
+++ src/project/wells/bond/pages/consultation/WwbncRentalResignExpectedMgtMExpected.vue
@@ -409,7 +409,7 @@ async function onClickFinalConfirm() {
   await onClickSearch();
 }
 
-const isNotExpected = computed(() => searchParams.value.authRsgCd === '01');
+// const isNotExpected = computed(() => searchParams.value.authRsgCd === '01');
 const isExpectedConfirm = computed(() => searchParams.value.authRsgCd === '02');
 const isfinalConfirm = computed(() => searchParams.value.authRsgCd === '03');
 // TODO: 룰 추가 예정 ( 현재 시스템 룰, 집금담당자, 'DUMMY' 적용 )
@@ -451,7 +451,7 @@ const initExpectedGrid = defineGrid((data, view) => {
         const ret = {};
         const { excdYn } = grid.getValues(dataCell.index.itemIndex);
         const rowState = gridUtil.getRowState(grid, dataCell.index.dataRow);
-        if (rowState === RowState.UPDATED && excdYn === 'N' && isNotExpected.value && !isLastDate.value) {
+        if (rowState === RowState.UPDATED && excdYn === 'N' && !isLastDate.value) {
           ret.editable = false;
           grid.setValue(dataCell.index.itemIndex, 'authRsgExcdRsonCd', '');
         } else if (isfinalConfirm.value) {
