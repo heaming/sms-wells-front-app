2ab82879d976a5f3f4aa491a9669af2e5d527137
gs.piit129 <gs.piit129@kyowon.co.kr>
2023-08-23 11:02:20 +0900

    chore : 수정

--- src/project/wells/service/pages/visit/WwsnbSafetyAccidentMgtM.vue
+++ src/project/wells/service/pages/visit/WwsnbSafetyAccidentMgtM.vue
@@ -248,10 +248,11 @@ import {
 } from 'kw-lib';
 import dayjs from 'dayjs';
 import { cloneDeep, isEmpty } from 'lodash-es';
+import { openReportPopup } from '~common/utils/cmPopupUtil';
 
 const { getConfig } = useMeta();
 const dataService = useDataService();
-const { modal, alert, notify } = useGlobal();
+const { modal, notify } = useGlobal();
 const grdMainRef = ref(getComponentType('KwGrid'));
 const { t } = useI18n();
 const { currentRoute } = useRouter();
@@ -321,13 +322,34 @@ async function fetchData() {
 }
 
 async function onClickSearch() {
+  const splited = (searchParams.value.cntrDtlNo).split('-');
+  searchParams.value.cntrNo = splited[0];
+  searchParams.value.cntrSn = splited[1];
+
   pageInfo.value.pageIndex = 1;
   cachedParams = cloneDeep(searchParams.value);
   await fetchData();
 }
 
 async function onClickAgreementPrint() {
-  await alert('합의서개발전');
+  const view = grdMainRef.value.getView();
+  const chkRows = gridUtil.getCheckedRowValues(view);
+  const { acdnRcpId } = chkRows[0];
+  const ozParam = ref({
+    args: {
+      searchApiUrl: `/api/v1/anonymous/sms/wells/service/safety-accident-agreement/${acdnRcpId}`,
+    },
+    height: 1100,
+    width: 1200,
+  });
+
+  // 조회 팝업
+  openReportPopup(
+    '/kyowon_as/safetyAccidentAgr.ozr',
+    '/kyowon_as/safetyAccidentAgr.odi',
+    JSON.stringify(ozParam.value.args),
+    { width: ozParam.width, height: ozParam.height },
+  );
 }
 
 async function onClickAccidentReportPrint() {
