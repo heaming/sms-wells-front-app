9739d904b24c5f6bb569155dbef8925f112cc2f9
jungheejin <gs.piit227@kyowon.co.kr>
2023-09-12 21:03:42 +0900

    feat: 대리점지역별 필터발주현황 소스 수정

--- src/project/wells/service/pages/stock/WwsnaAgencyLocaraFilterGiveAOrderPsListM.vue
+++ src/project/wells/service/pages/stock/WwsnaAgencyLocaraFilterGiveAOrderPsListM.vue
@@ -49,6 +49,32 @@
         :total-count="totalCount"
         @init="initGrdMain"
       />
+      <kw-action-top>
+        <template #left>
+          <kw-paging-info
+            v-model:page-index="pageInfo.pageIndex"
+            v-model:page-size="pageInfo.pageSize"
+            v-model:total-count="pageInfo.totalCount"
+            v-model:page-size-options="codes.COD_PAGE_SIZE_OPTIONS"
+            @change="fetchData2"
+          />
+          <span class="ml8">{{ t('MSG_TXT_UNIT_WON') }}</span>
+        </template>
+        <kw-btn
+          icon="download_on"
+          dense
+          secondary
+          :label="$t('MSG_BTN_EXCEL_DOWN')"
+          :disable="pageInfo.totalCount === 0"
+          @click="onClickExcelDownload2"
+        />
+      </kw-action-top>
+      <kw-grid
+        ref="grdMainRef2"
+        :page-size="pageInfo.pageSize"
+        :total-count="pageInfo.totalCount"
+        @init="initGrdMain2"
+      />
     </div>
   </kw-page>
 </template>
@@ -60,10 +86,11 @@
 // -------------------------------------------------------------------------------------------------
 
 import { getComponentType, defineGrid, gridUtil, useDataService, codeUtil } from 'kw-lib';
-import { cloneDeep } from 'lodash-es';
+import { cloneDeep, isEmpty } from 'lodash-es';
 import dayjs from 'dayjs';
 
 const grdMainRef = ref(getComponentType('KwGrid'));
+const grdMainRef2 = ref(getComponentType('KwGrid'));
 const dataService = useDataService();
 
 const { t } = useI18n();
@@ -75,16 +102,14 @@ const now = dayjs();
 // -------------------------------------------------------------------------------------------------
 const baseUrl = '/sms/wells/service/agency-locara-filter-give-a-order-ps';
 
-// const codes =
-await codeUtil.getMultiCodes(
-  'SV_BIZ_DCLSF_CD',
+const codes = await codeUtil.getMultiCodes(
+  'COD_PAGE_SIZE_OPTIONS',
 );
-
-// const customCodes = {
-//   ex: [
-//     { codeId: '1234', codeName: '5678' },
-//   ],
-// };
+const pageInfo = ref({
+  totalCount: 0,
+  pageIndex: 1,
+  pageSize: 10,
+});
 
 const searchParams = ref({
   startDt: now.format('YYYYMM'),
@@ -93,19 +118,33 @@ const searchParams = ref({
 let cachedParams;
 const totalCount = ref(0);
 
-async function fetchData() {
-  const res = await dataService.get(`${baseUrl}/agrg`, { params: cachedParams });
-  const list = res.data;
-  totalCount.value = list.length;
+// async function fetchData1() {
+//   const res = await dataService.get(`${baseUrl}/agrg`, { params: cachedParams });
+//   const list = res.data;
+//   totalCount.value = list.length;
+//
+//   const view = grdMainRef.value.getView();
+//   view.getDataSource().setRows(list);
+//   view.resetCurrent();
+// }
+async function fetchData2() {
+  const res = await dataService.get(`${baseUrl}/paging`, { params: { ...cachedParams, ...pageInfo.value } });
+  const { list: pages, pageInfo: pagingResult } = res.data;
 
-  const view = grdMainRef.value.getView();
-  view.getDataSource().setRows(list);
+  pageInfo.value = pagingResult;
+
+  console.log(pages);
+
+  const view = grdMainRef2.value.getView();
+  view.getDataSource().setRows(pages);
   view.resetCurrent();
 }
 async function onClickSearch() {
   cachedParams = cloneDeep(searchParams.value);
-  await fetchData();
+  // await fetchData1();
+  await fetchData2();
 }
+
 async function onClickExcelDownload() {
   const view = grdMainRef.value.getView();
   await gridUtil.exportView(view, {
@@ -114,10 +153,20 @@ async function onClickExcelDownload() {
     exportData: gridUtil.getAllRowValues(view),
   });
 }
+async function onClickExcelDownload2() {
+  const view = grdMainRef2.value.getView();
+  const res = await dataService.get(`${baseUrl}/excel-download`, { params: cachedParams });
+  await gridUtil.exportView(view, {
+    fileName: currentRoute.value.meta.menuName,
+    timePostfix: true,
+    exportData: res.data,
+  });
+}
 // -------------------------------------------------------------------------------------------------
 // Initialize Grid
 // -------------------------------------------------------------------------------------------------
 
+// 필터발주 현황
 const initGrdMain = defineGrid((data, view) => {
   const columns = [
     { fieldName: 'yymm', header: t('년월'), width: '80', styleName: 'text-center', datetimeFormat: 'yyyy-MM' },
@@ -132,6 +181,49 @@ const initGrdMain = defineGrid((data, view) => {
       dataType: 'number',
       numberFormat: '#,##0',
     },
+  ];
+  const fields = columns.map(({ fieldName, dataType }) => (dataType ? { fieldName, dataType } : { fieldName }));
+  data.setFields(fields);
+  view.setColumns(columns);
+  view.checkBar.visible = false;
+  view.rowIndicator.visible = true;
+});
+
+// 필터발주 상세현황
+const initGrdMain2 = defineGrid((data, view) => {
+  const columns = [
+    { fieldName: 'yymm', header: t('년월'), width: '80', styleName: 'text-center', datetimeFormat: 'yyyy-MM' },
+    { fieldName: 'cstKnm', header: t('업체명'), width: '80', styleName: 'text-center' },
+    { fieldName: 'cntrSn', visible: false },
+    {
+      fieldName: 'cntrNo',
+      header: t('MSG_TXT_CNTR_DTL_NO'),
+      width: '150',
+      styleName: 'text-center',
+      displayCallback(grid, index) {
+        const { cntrNo, cntrSn } = grid.getValues(index.itemIndex);
+        if (!isEmpty(cntrNo)) {
+          return `${cntrNo}-${cntrSn}`;
+        }
+      },
+    },
+    { fieldName: 'rcgvpKnm', header: t('MSG_TXT_CST_NM'), width: '100', styleName: 'text-center' },
+    { fieldName: 'pdNm', header: t('상품명'), width: '120', styleName: 'text-center' },
+    { fieldName: 'filtNm', header: t('MSG_TXT_ITM_NM'), width: '120', styleName: 'text-center' },
+    {
+      fieldName: 'sppBasAdr',
+      header: t('배송주소'),
+      width: '150',
+      styleName: 'text-left',
+      displayCallback(grid, index) {
+        const { sppBasAdr, sppDtlAdr } = grid.getValues(index.itemIndex);
+        if (!isEmpty(sppBasAdr)) {
+          return `${sppBasAdr}, ${sppDtlAdr}`;
+        }
+      },
+    },
+    { fieldName: 'sppDtlAdr', visible: false },
+    { fieldName: 'locaraDvNm', header: t('지역'), width: '120', styleName: 'text-center' },
   ];
   const fields = columns.map(({ fieldName, dataType }) => (dataType ? { fieldName, dataType } : { fieldName }));
   data.setFields(fields);
