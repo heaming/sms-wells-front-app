ac0e0cdac66a6e8c1e85a035cb759a84384472cd
gs.piit182 <gs.piit182@kyowon.com>
2023-09-14 10:58:56 +0900

    chore: 수정

--- src/project/wells/service/pages/visit/WwsnbIndividualServiceListM.vue
+++ src/project/wells/service/pages/visit/WwsnbIndividualServiceListM.vue
@@ -778,9 +778,7 @@ async function isValidateIndividualParams() {
 
 async function onClickSearch() {
   if (isEmpty(searchParams.value.cntrNo) && isEmpty(searchParams.value.bcNo)) { notify(t('MSG_ALT_SRCH_CNDT_NEED_ONE_AMONG', [`${t('MSG_TXT_CNTR_DTL_NO')}, ${t('MSG_TXT_BARCODE')}`])); return; }
-  if (searchParams.value.cntrNo) {
-    if (searchParams.value.cntrNo.length < 12 || searchParams.value.cntrSn.length < 0) { return; }
-  }
+  if (searchParams.value.cntrNo) { if (searchParams.value.cntrSn.length < 0) { return; } }
   await isValidateIndividualParams();
 }
 
