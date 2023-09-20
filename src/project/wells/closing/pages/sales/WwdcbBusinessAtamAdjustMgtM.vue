cea4e004a9dbbd0a784cd01cb55bbb078601e84f
우승민 <gs.piit183@kyowon.co.kr>
2023-09-20 14:39:11 +0900

    fix: 버튼 권한 적용

--- src/project/wells/closing/pages/sales/WwdcbBusinessAtamAdjustMgtM.vue
+++ src/project/wells/closing/pages/sales/WwdcbBusinessAtamAdjustMgtM.vue
@@ -87,6 +87,7 @@
           <span class="ml8">{{ t('MSG_TXT_UNIT_WON') }}</span>
         </template>
         <kw-btn
+          v-permission:download
           icon="download_on"
           :disable="totalCount === 0"
           dense
@@ -100,6 +101,7 @@
           inset
         />
         <kw-btn
+          v-permission:update
           :disable="searchParams.searchGubun==='2'"
           secondary
           dense
@@ -112,6 +114,7 @@
           inset
         />
         <kw-btn
+          v-permission:create
           :disable="searchParams.searchGubun==='2'"
           primary
           dense
