981150e2388e4c577c1eb3fac73cdcf8611ed817
younuk.choi <gs.piit186@kyowon.co.kr>
2023-10-19 10:13:47 +0900

    fix :타임테이블 호출 파라미터 변경

--- src/project/wells/contract/pages/changeorder/WwctbRentalCancelRegistrationMgtM.vue
+++ src/project/wells/contract/pages/changeorder/WwctbRentalCancelRegistrationMgtM.vue
@@ -853,10 +853,10 @@ async function onClickRequidation() {
     componentProps: {
       baseYm: dayjs().format('YYYYMM'), // 달력 초기 월
       chnlDvCd: 'K', // W: 웰스, K: KSS, C: CubicCC, P: K-MEMBERS, I || E: 엔지니어, M: 매니저
-      svDvCd: '1', // 1:설치, 2:BS, 3:AS, 4:홈케어
-      sellDate: searchDetail.cntrCnfmDt, // // 판매일자
+      svDvCd: '3', // 1:설치, 2:BS, 3:AS, 4:홈케어
       svBizDclsfCd: '3420',
       mtrStatCd: '1',
+      sellDate: searchDetail.cntrCnfmDt,
       cntrNo: searchDetail.cntrNo,
       cntrSn: searchDetail.cntrSn,
     },
