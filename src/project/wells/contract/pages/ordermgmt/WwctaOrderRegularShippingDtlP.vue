f85ac1372c3cdefc8a5b1cc5a5aeb1cb70258d53
jihoon.kim <gs.piit193@kyowon.co.kr>
2023-10-30 10:59:25 +0900

    fix: 주문상세팝업(복합상품목록) 수정

--- src/project/wells/contract/pages/ordermgmt/WwctaOrderRegularShippingDtlP.vue
+++ src/project/wells/contract/pages/ordermgmt/WwctaOrderRegularShippingDtlP.vue
@@ -1260,9 +1260,9 @@ async function fetchData() {
     // -------------------------------------------------------------------------------------------------
     // 상품 정보
     // -------------------------------------------------------------------------------------------------
-    frmMainData.value.pdQty = pages[0].pdQty; // 수량
-    frmMainData.value.fnlVal = pages[0].fnlVal; // 단가
-    frmMainData.value.ctrVal = pages[0].ctrVal; // 할인
+    // frmMainData.value.pdQty = pages[0].pdQty; // 수량
+    // frmMainData.value.fnlVal = pages[0].fnlVal; // 단가
+    // frmMainData.value.ctrVal = pages[0].ctrVal; // 할인
     frmMainData.value.pdTpCm = pages[0].pdTpCm; // 제품선택유형
     frmMainData.value.stplPtrm = pages[0].stplPtrm; // 의무기간
     frmMainData.value.cntrPtrm = pages[0].cntrPtrm; // 계약기간
@@ -1341,8 +1341,22 @@ async function fetchData() {
 
   res = await dataService.get('/sms/wells/contract/contracts/order-detail-mngt/regular-shippings/composition-products', { params: { ...cachedParams, ...pageInfo.value } });
 
-  const view = grdMainRef.value.getView();
+  let totPdQty = 0;
+  let totFnlVal = 0;
+  let totCtrVal = 0;
+
+  res.data.forEach((rowData) => {
+    totPdQty += Number(rowData.pdQty);
+    totFnlVal += Number(rowData.fnlVal);
+    totCtrVal += Number(rowData.ctrVal);
+  });
+
+  frmMainData.value.pdQty = stringUtil.getNumberWithComma(Number(totPdQty), 0);
+  frmMainData.value.fnlVal = stringUtil.getNumberWithComma(Number(totFnlVal), 0);
+  frmMainData.value.ctrVal = stringUtil.getNumberWithComma(Number(totCtrVal), 0);
+
   // view.getDataSource().setRows(accounts);
+  const view = grdMainRef.value.getView();
   view.getDataSource().setRows(res.data);
   pageInfo.value.totalCount = view.getItemCount();
   view.resetCurrent();
@@ -1362,13 +1376,13 @@ async function onClickConfirm() {
 // -------------------------------------------------------------------------------------------------
 const initGrid = defineGrid((data, view) => {
   const fields = [
-    { fieldName: 'basePdCd' },
-    { fieldName: 'pdNm' },
-    { fieldName: 'pdQty' },
-    { fieldName: 'fnlVal' },
-    { fieldName: 'ctrVal' },
-    { fieldName: 'ojPdCd' },
-    { fieldName: 'verSn' },
+    { fieldName: 'basePdCd' }, // 상품코드
+    { fieldName: 'pdNm' }, // 상품명
+    { fieldName: 'pdQty' }, // 수량
+    { fieldName: 'fnlVal', dataType: 'number' }, // 단가
+    { fieldName: 'ctrVal', dataType: 'number' }, // 할인
+    { fieldName: 'ojPdCd' }, // 패키지 코드
+    { fieldName: 'verSn' }, // 패키지 가격 차수
   ];
 
   const columns = [
