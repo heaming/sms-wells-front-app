6aa922e5fbba2e912a37d07331bfbfb4245c8f0e
choi sung hun <gs.piit151@kyowon.com>
2023-10-30 10:11:16 +0900

    chore : 결함수정

--- src/project/wells/bond/pages/consultation/WwbncSameCustomerContractPSales.vue
+++ src/project/wells/bond/pages/consultation/WwbncSameCustomerContractPSales.vue
@@ -13,8 +13,12 @@
 ****************************************************************************************************
 --->
 <template>
-  <kw-action-top class="mt20" />
+  <kw-action-top
+    v-show="rental||msh||spay"
+    class="mt20"
+  />
   <kw-form
+    v-show="rental"
     :cols="4"
     dense
     class="kw-form--small"
@@ -24,28 +28,28 @@
         :label="$t('MSG_TXT_REG_FEE')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.regFee) }}
+          {{ sales.rentalRgstCost }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_RTLFE1_MM_CHRAM')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.rtlfe1MmChram) }}
+          {{ sales.rentalAmt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_RTLFE_DUTY_PTRM')"
       >
         <p>
-          {{ sales.rtlfeDutyPtrm }}
+          {{ sales.dutyUseMcn }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_REQD_RQDT')"
       >
         <p>
-          {{ sales.reqdRqdt }}
+          {{ sales.reqdDtm }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -54,28 +58,28 @@
         :label="$t('MSG_TXT_RGST_COST_DSC')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.rgstCostDsc) }}
+          {{ sales.rgstDsc }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_RTLFE1_DSC')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.rtlfe1Dsc) }}
+          {{ sales.rentalDscAmt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_PD_DC_CLASS')"
       >
         <p>
-          {{ sales.pdDcClass }}
+          {{ sales.sellDscDvNm }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_CANC_DT')"
       >
         <p>
-          {{ sales.cancDt }}
+          {{ sales.canDt }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -84,21 +88,21 @@
         :label="$t('MSG_TXT_RNTL_TOTAL')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.rntlTotal) }}
+          {{ sales.cntrTam }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_RTLFE1_MCNT')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.rtlfe1Mcnt) }}
+          {{ sales.rentalAmt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_DISC_CODE')"
       >
         <p>
-          {{ sales.discCode }}
+          {{ sales.sellDscTpNm }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -107,21 +111,21 @@
         :label="$t('MSG_TXT_LEASE_DV')"
       >
         <p>
-          {{ sales.leaseDv }}
+          {{ sales.sellTpNm }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_RTLFE2_MM_CHRAM')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.rtlfe2MmChram) }}
+          {{ sales.rentalAmt2 }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_MNGT_PRD_MCNT')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.mngtPrdMcnt) }}
+          {{ sales.svPrd }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -130,21 +134,21 @@
         :label="$t('MSG_TXT_1PLUS1_LK_CNTR')"
       >
         <p>
-          {{ sales.onePlusOneLkCntr }}
+          {{ sales.linkDtlCntrNo }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_RTLFE2_DSC')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.rtlfe2Dsc) }}
+          {{ sales.rentalDscAmt2 }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_PAR_CNTR')"
       >
         <p>
-          {{ sales.parCntr }}
+          {{ sales.sdingDtlCntrNo }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -153,28 +157,29 @@
         :label="$t('MSG_TXT_ALNC_LK_CNTR')"
       >
         <p>
-          {{ sales.alncLkCntr }}
+          {{ sales.alncLinkDtlCntrNo }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_RTLFE2_MCNT')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.rtlfe2Mcnt) }}
+          {{ sales.rentalPtrm2 }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_SL_DT')"
       >
         <p>
-          {{ sales.slDt }}
+          {{ sales.slRcogDt }}
         </p>
       </kw-form-item>
     </kw-form-row>
   </kw-form>
-  <kw-separator />
+
   <kw-action-top />
   <kw-form
+    v-show="msh"
     :cols="4"
     dense
     class="kw-form--small"
@@ -184,14 +189,14 @@
         :label="$t('MSG_TXT_MM_SSPCS')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.mmSspcs) }}
+          {{ sales.sellAmt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_DISC_CODE')"
       >
         <p>
-          {{ sales.discCode }}
+          {{ sales.sellDscTpNm }}
         </p>
       </kw-form-item>
       <kw-form-item
@@ -202,10 +207,10 @@
         </p>
       </kw-form-item>
       <kw-form-item
-        :label="$t('MSG_TXT_VST_PRD')"
+        :label="$t('MSG_TXT_SVC_BETWEEN')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.vstPrd) }}
+          {{ sales.svPrd2 }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -214,21 +219,21 @@
         :label="$t('MSG_TXT_STPL_MCNT')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.stplMcnt) }}
+          {{ sales.stplPtrm }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_DSC_AMT')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.dscAmt) }}
+          {{ sales.dscAmt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_VST_DT')"
       >
         <p>
-          {{ sales.vstDt }}
+          {{ sales.sppDuedt }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -237,29 +242,29 @@
         :label="$t('MSG_TXT_MNGT_PRD')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.mngtPrd) }}
+          {{ sales.svPrd2 }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_J_DT')"
       >
         <p>
-          {{ sales.jDt }}
+          {{ sales.cntrDt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_VST_NMN')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.vstNmn) }}
+          {{ sales.vstNmnN }}
         </p>
       </kw-form-item>
     </kw-form-row>
   </kw-form>
 
-  <kw-separator />
   <kw-action-top />
   <kw-form
+    v-show="spay"
     :cols="4"
     dense
     class="kw-form--small"
@@ -269,28 +274,28 @@
         :label="$t('MSG_TXT_FRISU_MSH')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.frisuMsh) }}
+          {{ sales.frisuBfsvcPtrmN }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_SALE_PRICE')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.salePrice) }}
+          {{ sales.sellAmt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_TK_AMT')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.tkAmt) }}
+          {{ sales.cntrAmt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_ISTM_MCNT')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.istmMcnt) }}
+          {{ sales.istmMcn }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -299,21 +304,21 @@
         :label="$t('MSG_TXT_FRISU_AS')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.frisuAs) }}
+          {{ sales.frisuAsPtrmN }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_CNTRCT_AMT')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.cntrctAmt) }}
+          {{ sales.fnlAmt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_ISTM_AMT')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.istmAmt) }}
+          {{ sales.istmPcamAmt }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -322,21 +327,21 @@
         :label="$t('MSG_TXT_RECAP_MSH')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.recapMsh) }}
+          {{ sales.frisuAsPtrmN }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_SBSCM')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.sbscm) }}
+          {{ sales.cntrAmt }}
         </p>
       </kw-form-item>
       <kw-form-item
         :label="$t('MSG_TXT_MM_INTAM')"
       >
         <p>
-          {{ stringUtil.getNumberWithComma(sales.mmIntam) }}
+          {{ sales.mmIstmAmt }}
         </p>
       </kw-form-item>
     </kw-form-row>
@@ -347,15 +352,14 @@
 // -------------------------------------------------------------------------------------------------
 // Import & Declaration
 // -------------------------------------------------------------------------------------------------
-import { useDataService, stringUtil, codeUtil } from 'kw-lib';
+import { useDataService, stringUtil } from 'kw-lib';
+import { isEmpty } from 'lodash-es';
+import dayjs from 'dayjs';
 
 const dataService = useDataService();
-const hsmtrl = ref();
-const cocn = ref();
-
-const codes = await codeUtil.getMultiCodes(
-  'SELL_TP_CD',
-);
+const rental = ref();
+const msh = ref();
+const spay = ref();
 
 const props = defineProps({
   selectedGridRow: {
@@ -367,85 +371,56 @@ const props = defineProps({
 // -------------------------------------------------------------------------------------------------
 // Function & Event
 // -------------------------------------------------------------------------------------------------
-const paramData = ref({
-  sellTpNm: '',
-  sellTpCd: '',
-  pdNm: '',
-  bndCntrRefId: '',
-  bndCntrRefSn: '',
-  cstKnm: '',
-  memberNm: '',
-  prtnrKnm: '',
-  mmIstmAmt: '',
-  disAmt: '',
-  purMmIntamAmt: '',
-  dlqBlam: '',
-  thmBlam: '',
-  mpyBsdt: '',
-  dlqMcn: '',
-});
+const paramData = ref({});
+const sales = ref({});
 
-const sales = ref({
-  regFee: '',
-  rtlfe1MmChram: '',
-  rtlfeDutyPtrm: '',
-  reqdRqdt: '',
-  rgstCostDsc: '',
-  rtlfe1Dsc: '',
-  pdDcClass: '',
-  cancDt: '',
-  rntlTotal: '',
-  rtlfe1Mcnt: '',
-  discCode: '',
-  leaseDv: '',
-  rtlfe2MmChram: '',
-  mngtPrdMcnt: '',
-  onePlusOneLkCntr: '',
-  rtlfe2Dsc: '',
-  parCntr: '',
-  alncLkCntr: '',
-  rtlfe2Mcnt: '',
-  slDt: '',
-  mmSspcs: '',
-  discCode2: '',
-  wdwalDt: '',
-  vstPrd: '',
-  stplMcnt: '',
-  dscAmt: '',
-  vstDt: '',
-  mngtPrd: '',
-  jDt: '',
-  vstNmn: '',
-  frisuMsh: '',
-  salePrice: '',
-  tkAmt: '',
-  istmMcnt: '',
-  frisuAs: '',
-  cntrctAmt: '',
-  istmAmt: '',
-  recapMsh: '',
-  sbscm: '',
-  mmIntam: '',
-});
-
-function setSellTpCd(sellTpCd) {
-  if (sellTpCd === codes.SELL_TP_CD[0].codeId) {
-    hsmtrl.value = false;
-    cocn.value = true;
+function setBndBizDvCd(bndBizDvCd) {
+  if (bndBizDvCd === 'L20' || bndBizDvCd === 'L21') {
+    rental.value = true;
+    msh.value = false;
+    spay.value = false;
+  } else if (bndBizDvCd === 'L30') {
+    rental.value = false;
+    msh.value = true;
+    spay.value = false;
   } else {
-    hsmtrl.value = true;
-    cocn.value = false;
+    rental.value = false;
+    msh.value = false;
+    spay.value = true;
   }
 }
 
 async function fetchData() {
-  const res = await dataService.get(`/sms/wells/bond/same-customer-contracts/${paramData.value.sellTpCd}/sales`);
+  const res = await dataService.get('/sms/wells/bond/same-customer-contracts/sales', { params: paramData.value });
   sales.value = res.data;
+
+  sales.value.rentalRgstCost = stringUtil.getNumberWithComma(sales.value.rentalRgstCost);
+  sales.value.rentalAmt = stringUtil.getNumberWithComma(sales.value.rentalAmt);
+  sales.value.reqdDtm = !isEmpty(sales.value.reqdDtm) ? dayjs(sales.value.reqdDtm).format('YYYY-MM-DD') : '';
+  sales.value.rgstDsc = stringUtil.getNumberWithComma(sales.value.rgstDsc);
+  sales.value.rentalDscAmt = stringUtil.getNumberWithComma(sales.value.rentalDscAmt);
+  sales.value.canDt = !isEmpty(sales.value.canDt) ? dayjs(sales.value.canDt).format('YYYY-MM-DD') : '';
+  sales.value.cntrTam = stringUtil.getNumberWithComma(sales.value.cntrTam);
+  sales.value.rentalAmt = stringUtil.getNumberWithComma(sales.value.rentalAmt);
+  sales.value.rentalAmt2 = stringUtil.getNumberWithComma(sales.value.rentalAmt2);
+  sales.value.rentalDscAmt2 = stringUtil.getNumberWithComma(sales.value.rentalDscAmt2);
+  sales.value.rentalPtrm2 = stringUtil.getNumberWithComma(sales.value.rentalPtrm2);
+  sales.value.slRcogDt = !isEmpty(sales.value.slRcogDt) ? dayjs(sales.value.slRcogDt).format('YYYY-MM-DD') : '';
+
+  sales.value.sellAmt = stringUtil.getNumberWithComma(sales.value.sellAmt);
+  sales.value.dscAmt = stringUtil.getNumberWithComma(sales.value.dscAmt);
+  sales.value.sppDuedt = !isEmpty(sales.value.sppDuedt) ? dayjs(sales.value.sppDuedt).format('YYYY-MM-DD') : '';
+  sales.value.cntrDt = !isEmpty(sales.value.cntrDt) ? dayjs(sales.value.cntrDt).format('YYYY-MM-DD') : '';
+
+  sales.value.cntrAmt = stringUtil.getNumberWithComma(sales.value.cntrAmt);
+  sales.value.fnlAmt = stringUtil.getNumberWithComma(sales.value.fnlAmt);
+  sales.value.istmPcamAmt = stringUtil.getNumberWithComma(sales.value.istmPcamAmt);
+  sales.value.mmIstmAmt = stringUtil.getNumberWithComma(sales.value.mmIstmAmt);
 }
 
 watch(() => props.selectedGridRow, async (obj) => {
   if (obj) {
-    setSellTpCd(obj.sellTpCd);
+    setBndBizDvCd(obj.bndBizDvCd);
     paramData.value = obj;
     await fetchData();
   }
