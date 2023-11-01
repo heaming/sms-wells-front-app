c565e23dea8e75ea7346155cf66164ae556af2ed
김민석 <gs.piit258@kyowon.co.kr>
2023-11-01 10:38:29 +0900

    fix: 총판취소고객현황조회 oz리포트 추가

--- src/project/wells/contract/pages/changeorder/WwctbSoleDistributorCancelCntrListM.vue
+++ src/project/wells/contract/pages/changeorder/WwctbSoleDistributorCancelCntrListM.vue
@@ -74,14 +74,18 @@
 // -------------------------------------------------------------------------------------------------
 // Import & Declaration
 // -------------------------------------------------------------------------------------------------
-import { codeUtil, useMeta, useDataService, getComponentType, useGlobal, defineGrid, gridUtil } from 'kw-lib';
-import { cloneDeep } from 'lodash-es';
+import { codeUtil, defineGrid, gridUtil, getComponentType, stringUtil, useMeta, useDataService, useGlobal } from 'kw-lib';
+import { isEmpty, cloneDeep } from 'lodash-es';
 import dayjs from 'dayjs';
+import { openReportPopup } from '~common/utils/cmPopupUtil';
 
+const { getters } = useStore();
 const { getConfig } = useMeta();
 const { t } = useI18n();
 const dataService = useDataService();
 const { notify } = useGlobal();
+const userInfo = getters['meta/getUserInfo'];
+
 // -------------------------------------------------------------------------------------------------
 // Function & Event
 // -------------------------------------------------------------------------------------------------
@@ -127,8 +131,49 @@ async function onClickSearch() {
 }
 
 async function ozPrint() {
-  notify(t('리포트작업은 예정입니다.'));
+  if (isEmpty(cachedParams)) { // 조회 조건이 없다 (조회없이 누를경우)
+    notify('검색 결과가 없습니다.');
+    return;
+  }
+
+  const res = await dataService.get('/sms/wells/contract/contracts/soledistributor-cancel-contracts/ozReporting', { params: { ...cachedParams, ...pageInfo.value } });
+  // console.log(res.data);
+
+  const mapRes = res.data.map((v) => ({ // data naming 수정(ozReport(AS-IS)기준으로)
+    hdqr: v.ogCd, // 소속
+    hqldNm: v.hooPrtnrNm, // 본부장명
+    custCd: v.cntrNo, // 계약번호
+    LCCNAM: v.cstKnm, // 고객명
+    LCW_HPNO: !isEmpty(v.telNo) ? `${v.istCralLocaraTno}${v.istMexnoEncr}${v.istCralIdvTno}` : '', // 설치자 휴대번호
+    LCW_ADDR: `${v.istRnadr} ${v.istRdadr}`, // 설치자 주소
+    prdtNm: v.pdAbbrNm, // 상품명
+    setDt: !isEmpty(v.canDt) ? stringUtil.getDateFormat(v.istDt, '-') : '', // 설치일자
+    cancelDt: !isEmpty(v.canDt) ? stringUtil.getDateFormat(v.canDt, '-') : '', // 취소일자
+    contDesc: !isEmpty(v.canCn) ? v.canCn : '', // 취소내용
+  }));
+
+  // console.log(cachedParams);
+  const pritChpr = `${userInfo.userName}(${userInfo.employeeIDNumber})`; // 조회계정정보
+  const srchPerd = `${stringUtil.getDateFormat(cachedParams.canStrtDt, 'YYYY.MM.DD')}  ~ ${stringUtil.getDateFormat(cachedParams.canEndDt, 'YYYY.MM.DD')}`; // 취소일자
+  const codeNm = codes.PDGRP_ACD.filter((x) => cachedParams.pdGbn.includes(x.codeId)); // 조회상품구분
+  const srchDiv = !isEmpty(codeNm) ? codeNm[0].codeName : t('MSG_TXT_ALL');
+
+  console.log(codeNm);
+  console.log(srchDiv);
+  await openReportPopup(
+    '/kstation-w/acrs/cancelCust.ozr',
+    null,
+    JSON.stringify(
+      {
+        jsondata: JSON.stringify(mapRes),
+        pritChpr,
+        srchPerd,
+        srchDiv,
+      },
+    ),
+  );
 }
+
 // -------------------------------------------------------------------------------------------------
 // Initialize Grid
 // -------------------------------------------------------------------------------------------------
@@ -136,16 +181,21 @@ const initGridSoleDistributorCanCntrList = defineGrid((data, view) => {
   const fields = [
     { fieldName: 'ogCd' }, // 소속
     { fieldName: 'hooPrtnrNm' }, // 본부장명
+    { fieldName: 'hooPrtnrNmMsk' }, // 본부장명 마스킹
     { fieldName: 'cntrNo' }, // 계약번호
     { fieldName: 'cntrSn' }, // 계약 일련번호
     { fieldName: 'cstKnm' }, // 계약자 한글명
+    { fieldName: 'cstKnmMsk' }, // 계약자 한글명 마스킹
     { fieldName: 'telNo' }, // 설치자 휴대전화번호
+    { fieldName: 'telNoMsk' }, // 설치자 휴대전화번호 마스킹
     { fieldName: 'istCralLocaraTno' }, // 설치자 휴대전화번호1
     { fieldName: 'istMexnoEncr' }, // 설치자 휴대전화번호2
     { fieldName: 'istCralIdvTno' }, // 설치자 휴대전화번호3
     { fieldName: 'istAdrZip' }, // 설치자정보 우편번호
+    { fieldName: 'istaddr' }, // 설치자정보 주소
     { fieldName: 'istRnadr' }, // 설치자정보 기준주소
     { fieldName: 'istRdadr' }, // 설치자정보 상세주소
+    { fieldName: 'istRdadrMsk' }, // 설치자정보 상세주소 마스킹
     { fieldName: 'pdClsfNm' }, // 구분 - 중분류
     { fieldName: 'pdMclsfRnm' }, // 구분
     { fieldName: 'pdAbbrNm' }, // 상품약어명
@@ -157,16 +207,24 @@ const initGridSoleDistributorCanCntrList = defineGrid((data, view) => {
 
   const columns = [
     { fieldName: 'ogCd', header: t('MSG_TXT_BLG'), width: '109', styleName: 'text-center' },
-    { fieldName: 'hooPrtnrNm', styleName: 'text-center', header: t('MSG_TXT_GNR_MNG'), width: '96' }, // 본부장
+    { fieldName: 'hooPrtnrNmMsk', styleName: 'text-center', header: t('MSG_TXT_GNR_MNG'), width: '96' }, // 본부장
     { fieldName: 'cntrNo', header: t('MSG_TXT_CNTR_NO'), width: '132', styleName: 'text-center' },
-    { fieldName: 'cstKnm', styleName: 'text-center', header: t('MSG_TXT_CST_NM'), width: '96' }, // 고객병
+    { fieldName: 'cstKnmMsk', styleName: 'text-center', header: t('MSG_TXT_CST_NM'), width: '96' }, // 고객
     {
-      fieldName: 'telNo',
+      fieldName: 'telNoMsk',
       header: `${t('MSG_TXT_INSTR')} ${t('MSG_TXT_MPNO')}`,
       width: '178',
       styleName: 'text-center',
     },
-    { fieldName: 'istRnadr', header: `${t('MSG_TXT_INSTR')} ${t('MSG_TXT_ADDR')}`, width: '432' },
+    {
+      fieldName: 'istaddr',
+      header: `${t('MSG_TXT_INSTR')} ${t('MSG_TXT_ADDR')}`,
+      width: '432',
+      displayCallback(grid, index) {
+        const { istRnadr, istRdadrMsk } = grid.getValues(index.itemIndex);
+        return `${istRnadr} ${istRdadrMsk}`; // 기준주소 + 상세주소 마스킹
+      },
+    },
     { fieldName: 'pdMclsfRnm', header: t('MSG_TXT_DIV'), width: '96', styleName: 'text-center', options: codes.PDGRP_ACD },
     { fieldName: 'pdAbbrNm', header: t('MSG_TXT_PRDT_NM'), width: '202' },
     { fieldName: 'istDt', header: t('MSG_TXT_IST_DT'), width: '132', styleName: 'text-center', dataType: 'date', datetimeFormat: 'date' },
