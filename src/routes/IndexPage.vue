eb78c4c30fbe59d845e7e971470b1a56f9361c90
mingyu.kim <gs.piit166@kyowon.co.kr>
2023-11-02 14:28:20 +0900

    entry notice 추가

--- src/routes/IndexPage.vue
+++ src/routes/IndexPage.vue
@@ -26,10 +26,11 @@
 </template>
 
 <script setup>
-import { WebDashboardM, consts } from 'kw-lib';
+import { WebDashboardM, consts, http, useGlobal } from 'kw-lib';
 
 const { push } = useRouter();
 const store = useStore();
+const { modal } = useGlobal();
 
 const homecards = import.meta.globEager('../modules/common/components/homecard/*.vue');
 const components = shallowRef(
@@ -50,4 +51,20 @@ if (user.userId !== 'anonymous' && user.portalId !== 'NO_SESSION') {
   onClickShowHomeCard();
 }
 
+async function openEntryNotices() {
+  const res = await http.get('/sflex/common/common/entry-notice');
+  const modals = res.data;
+  modals.forEach((m) => {
+    modal({
+      component: `${m.pageSourceId}`,
+    }).catch(() => {
+      // 파일명이 없을 때 오류나면 그냥 아무것도 안띄워주는 것으로.
+      // 나중에 alert 처리할지?
+    });
+  });
+}
+
+onMounted(() => {
+  openEntryNotices();
+});
 </script>
