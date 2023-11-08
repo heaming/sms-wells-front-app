e5d305f9662bbcd412c6dbcaa48cfca96f9d977d
gs.piit160 <gs.piit160@kyowon.co.kr>
2023-11-08 18:04:58 +0900

    fix: 결함 수정

--- src/project/wells/withdrawal/pages/bilfnt/WmwdaAutoTransferChangeMgtM.vue
+++ src/project/wells/withdrawal/pages/bilfnt/WmwdaAutoTransferChangeMgtM.vue
@@ -188,16 +188,16 @@ async function onClickAlarmSend() {
 }
 
 onMounted(async () => {
-  if (!window.opener) {
-    const path = '/mobile/#/withdrawal/wmwda-auto-transfer-change-mgt';
-    const size = {
-      width: 390,
-      height: 844,
-    };
+  // if (!window.opener) {
+  //   const path = '/mobile/#/withdrawal/wmwda-auto-transfer-change-mgt';
+  //   const size = {
+  //     width: 390,
+  //     height: 844,
+  //   };
 
-    await router.close(0, true);
-    await popupUtil.open(`${path}`, size, false);
-  }
+  //   await router.close(0, true);
+  //   await popupUtil.open(`${path}`, size, false);
+  // }
   formRef.value.reset();
 });
 </script>
