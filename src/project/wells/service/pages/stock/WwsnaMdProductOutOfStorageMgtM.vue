778bc8c7f3d8d41f0ae750a34506d7ba12e8a6b1
jungheejin <gs.piit227@kyowon.co.kr>
2023-11-29 19:25:29 +0900

    chore :MD 출고관리 수정사항 반영   MD 출고관리   - 검색조건 (파트너업체) 로그인 사번 기준으로 설정   - 택배송장-> 송장양식 명칭 변경

--- src/project/wells/service/pages/stock/WwsnaMdProductOutOfStorageMgtM.vue
+++ src/project/wells/service/pages/stock/WwsnaMdProductOutOfStorageMgtM.vue
@@ -100,9 +100,9 @@
         <kw-search-item :label="$t('MSG_TXT_PRTNR_BZS')">
           <kw-select
             v-model="searchParams.prtnrBzsCd"
-            :options="codes.PRTNR_BZS_CD"
             :label="$t('MSG_TXT_PRTNR_BZS_CD')"
             first-option="all"
+            readonly
           />
         </kw-search-item>
         <kw-search-item
@@ -132,7 +132,7 @@
           icon="download_on"
           dense
           secondary
-          :label="$t('MSG_BTN_ULD_SMP_FORM')"
+          :label="$t('MSG_TXT_IVC_FORM')"
           @click="onClickExcelDownload2"
         />
         <!-- 엑셀업로드 -->
@@ -286,7 +286,7 @@ async function onClickExcelDownload() {
 async function onClickExcelDownload2() {
   const view = grdSppIvcRef.value.getView();
   await gridUtil.exportView(view, {
-    fileName: `${t('MSG_BTN_ULD_SMP_FORM')}`,
+    fileName: `${t('MSG_TXT_IVC_FORM')}`,
     timePostfix: true,
     exportData: gridUtil.getAllRowValues(view),
   });
@@ -386,6 +386,19 @@ async function onClickExcelUpload() {
     await onClickSearch();
   }
 }
+async function getLoginPrtnrBzs() {
+  // 로그인된 사용자기준으로 파트너업체를 설정한다.
+  const res = await dataService.get(`${baseUrl}/login-prtnr-bzs`, { params: cachedParams });
+  const result = res.data;
+  console.log(result);
+  if (result !== '') {
+    searchParams.value.prtnrBzsCd = result;
+  }
+}
+
+onMounted(async () => {
+  await getLoginPrtnrBzs();
+});
 
 // -------------------------------------------------------------------------------------------------
 // Initialize Grid
@@ -540,7 +553,7 @@ const initGrdMain = defineGrid((data, view) => {
   view.rowIndicator.visible = true;
 });
 
-// 택배 송장 Grid
+// 송장양식 Grid
 const initGrdSppIvc = defineGrid((data, view) => {
   const columns = [
     { fieldName: 'cstSvAsnNo', header: t('MSG_TXT_ASGN_NO'), width: 160 },
